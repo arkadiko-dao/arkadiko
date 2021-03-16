c4ad9dc7730b994b9dd6e196bfb34a48bc70f523
Philip De Smedt <philip.desmedt@gmail.com>
2021-03-16 20:47:25 +0100

    Add link to proposal in governance

--- web/packages/web-app/components/proposal.tsx
+++ web/packages/web-app/components/proposal.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { ProposalProps } from './proposal-group';
+import { NavLink as RouterLink } from 'react-router-dom'
 
 export const Proposal: React.FC<ProposalProps> = ({ id, proposer, type, forVotes, token, against, changes }) => {
   return (
     <div>
-      <p>Proposal #{id} on token {token.toUpperCase()}</p>
+      <p>Proposal <RouterLink to={`governance/${id}`} exact className="px-2.5 py-1.5">{id}</RouterLink> on token {token.toUpperCase()}</p>
       <p>Proposer: {proposer}</p>
       <p>Type: {type}</p>
       <p>Votes for: {forVotes}</p>
