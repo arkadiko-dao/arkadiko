776e30fbf0e6edf151fcc4f60914c660153f52a0
Philip De Smedt <philip.desmedt@gmail.com>
2021-06-01 17:14:23 +0200

    wip

--- scripts/update-diko-price.js
+++ scripts/update-diko-price.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
-const CONTRACT_NAME = 'oracle';
+const CONTRACT_NAME = 'arkadiko-oracle-v1-1';
 const FUNCTION_NAME = 'update-price';
 const tx = require('@stacks/transactions');
 const BN = require('bn.js');
@@ -8,13 +8,16 @@ const utils = require('./utils');
 const network = utils.resolveNetwork();
 
 const updateDiko = async () => {
+  let nonce = await utils.getNonce(CONTRACT_ADDRESS);
+
   const txOptions = {
     contractAddress: CONTRACT_ADDRESS,
     contractName: CONTRACT_NAME,
     functionName: FUNCTION_NAME,
-    functionArgs: [tx.stringAsciiCV('DIKO'), tx.uintCV(new BN(0.1 * 100))],
+    functionArgs: [tx.stringAsciiCV('DIKO'), tx.uintCV(new BN(0.25 * 100))],
     senderKey: process.env.STACKS_PRIVATE_KEY,
     postConditionMode: 1,
+    nonce: new BN(nonce),
     network
   };
 
