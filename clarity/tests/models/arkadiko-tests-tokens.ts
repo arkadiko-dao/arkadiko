ba8f1ca6f3c0a1f0a8f009ff85701e23c628747a
Niel Deckx <contact@nieldeckx.be>
2021-06-02 09:17:45 +0200

    test: update auction engine tests to use new models (#1)
    Co-authored-by: Niel Deckx <>

---
+++ clarity/tests/models/arkadiko-tests-tokens.ts
@@ -0,0 +1,85 @@
+import {
+  Account,
+  Chain,
+  Clarinet,
+  Tx,
+  types,
+} from "https://deno.land/x/clarinet@v0.6.0/index.ts";
+
+
+// ---------------------------------------------------------
+// Oracle
+// ---------------------------------------------------------
+
+class OracleManager {
+  chain: Chain;
+  deployer: Account;
+
+  constructor(chain: Chain, deployer: Account) {
+    this.chain = chain;
+    this.deployer = deployer;
+  }
+
+  updatePrice(token: string, price: number) {
+    let block = this.chain.mineBlock([
+      Tx.contractCall("arkadiko-oracle-v1-1", "update-price", [
+        types.ascii(token),
+        types.uint(price),
+      ], this.deployer.address),
+    ]);
+    return block.receipts[0].result;
+  }
+}
+export { OracleManager };
+
+
+// ---------------------------------------------------------
+// xUSD
+// ---------------------------------------------------------
+
+class XusdManager {
+  chain: Chain;
+  deployer: Account;
+
+  constructor(chain: Chain, deployer: Account) {
+    this.chain = chain;
+    this.deployer = deployer;
+  }
+
+  balanceOf(wallet: string) {
+    return this.chain.callReadOnlyFn("xusd-token", "get-balance", [
+      types.principal(wallet),
+    ], this.deployer.address);
+  }
+  
+  totalSupply() {
+    return this.chain.callReadOnlyFn("xusd-token", "get-total-supply", [], this.deployer.address);
+  }
+}
+export { XusdManager };
+
+
+// ---------------------------------------------------------
+// xSTX
+// ---------------------------------------------------------
+
+class XstxManager {
+  chain: Chain;
+  deployer: Account;
+
+  constructor(chain: Chain, deployer: Account) {
+    this.chain = chain;
+    this.deployer = deployer;
+  }
+
+  balanceOf(wallet: string) {
+    return this.chain.callReadOnlyFn("xstx-token", "get-balance", [
+      types.principal(wallet),
+    ], this.deployer.address);
+  }
+  
+  totalSupply() {
+    return this.chain.callReadOnlyFn("xstx-token", "get-total-supply", [], this.deployer.address);
+  }
+}
+export { XstxManager };
