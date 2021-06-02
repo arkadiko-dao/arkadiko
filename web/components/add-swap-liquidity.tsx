c1070553244d1ee7dbabe60d09f156a0497e1dbc
Philip De Smedt <philip.desmedt@gmail.com>
2021-06-02 10:48:14 +0200

    wip: add liquidity

--- web/components/add-swap-liquidity.tsx
+++ web/components/add-swap-liquidity.tsx
@@ -13,11 +13,81 @@ import { tokenTraits } from '@common/vault-utils';
 
 export const AddSwapLiquidity: React.FC = ({ match }) => {
   const [state, setState] = useContext(AppContext);
-  const currencyA = match.params.currencyIdA;
-  const currencyB = match.params.currencyIdB;
-  const tokenXContract = tokenTraits[currencyA.toLowerCase()]['name'];
-  const tokenYContract = tokenTraits[currencyB.toLowerCase()]['name'];
+  const [balanceSelectedTokenX, setBalanceSelectedTokenX] = useState(0.0);
+  const [balanceSelectedTokenY, setBalanceSelectedTokenY] = useState(0.0);
+  const [tokenXAmount, setTokenXAmount] = useState(0.0);
+  const [tokenYAmount, setTokenYAmount] = useState(0.0);
+  const [currentPrice, setCurrentPrice] = useState(0.0);
+  const contractAddress = process.env.REACT_APP_CONTRACT_ADDRESS || '';
+  const stxAddress = useSTXAddress();
+
+  const tokenX = match.params.currencyIdA;
+  const tokenY = match.params.currencyIdB;
+  const tokenXContract = tokenTraits[tokenX.toLowerCase()]['name'];
+  const tokenYContract = tokenTraits[tokenY.toLowerCase()]['name'];
   websocketTxUpdater();
+
+  const setTokenBalances = () => {
+    setBalanceSelectedTokenX(microToReadable(state.balance[tokenX.toLowerCase()]));
+    setBalanceSelectedTokenY(microToReadable(state.balance[tokenY.toLowerCase()]));
+  };
+
+  useEffect(() => {
+    setTokenBalances();
+  }, [state.balance]);
+
+  useEffect(() => {
+    const fetchPair = async (tokenXContract:string, tokenYContract:string) => {
+      let details = await callReadOnlyFunction({
+        contractAddress,
+        contractName: "arkadiko-swap-v1-1",
+        functionName: "get-pair-details",
+        functionArgs: [
+          contractPrincipalCV(contractAddress, tokenXContract),
+          contractPrincipalCV(contractAddress, tokenYContract)
+        ],
+        senderAddress: stxAddress || '',
+        network: network,
+      });
+
+      return cvToJSON(details);
+    };
+
+    const resolvePair = async () => {
+      setTokenBalances();
+
+      const json3 = await fetchPair(tokenXContract, tokenYContract);
+      console.log('Pair Details:', json3);
+      if (json3['success']) {
+        const balanceX = json3['value']['value']['value']['balance-x'].value;
+        const balanceY = json3['value']['value']['value']['balance-y'].value;
+        const basePrice = (balanceX / balanceY).toFixed(2);
+        // const price = parseFloat(basePrice) + (parseFloat(basePrice) * 0.01);
+        setCurrentPrice(basePrice);
+      } else if (json3['value']['value']['value'] === 201) {
+        const json4 = await fetchPair(tokenYContract, tokenXContract);
+        if (json4['success']) {
+          const balanceX = json4['value']['value']['value']['balance-x'].value;
+          const balanceY = json4['value']['value']['value']['balance-y'].value;
+          const basePrice = (balanceX / balanceY).toFixed(2);
+          setCurrentPrice(basePrice);
+        }
+      }
+    };
+
+    resolvePair();
+  }, [tokenX, tokenY]);
+
+  const onInputChange = (event: { target: { name: any; value: any; }; }) => {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    setTokenXAmount(value);
+  };
+
+  const addLiquidity = () => {
+    console.log('add liquidity');
+  };
 
   return (
     <Box>
@@ -26,7 +96,7 @@ export const AddSwapLiquidity: React.FC = ({ match }) => {
           <main className="flex-1 relative pb-8 z-0 overflow-y-auto">
             <div className="mt-8">
               <h2 className="text-lg leading-6 font-medium text-gray-900 mb-4">
-                Add Liquidity on {currencyA}-{currencyB}
+                Add Liquidity on {tokenX}-{tokenY}
               </h2>
 
               <div className="mt-2 grid grid-cols-1 gap-5 sm:grid-cols-5 lg:grid-cols-5">
@@ -41,11 +111,11 @@ export const AddSwapLiquidity: React.FC = ({ match }) => {
                       <div className="ml-5 w-0 flex-1">
                         <dl>
                           <dt className="text-sm font-medium text-gray-500 truncate">
-                            {currencyA} balance
+                            {tokenX} balance
                           </dt>
                           <dd>
                             <div className="text-lg font-medium text-gray-900">
-                              {microToReadable(state.balance[currencyA.toLowerCase()])} STX
+                              {microToReadable(state.balance[tokenX.toLowerCase()])} STX
                             </div>
                           </dd>
                         </dl>
@@ -65,11 +135,11 @@ export const AddSwapLiquidity: React.FC = ({ match }) => {
                       <div className="ml-5 w-0 flex-1">
                         <dl>
                           <dt className="text-sm font-medium text-gray-500 truncate">
-                            {currencyB} balance
+                            {tokenY} balance
                           </dt>
                           <dd>
                             <div className="text-lg font-medium text-gray-900">
-                              {microToReadable(state.balance[currencyB.toLowerCase()])} xUSD
+                              {microToReadable(state.balance[tokenY.toLowerCase()])} xUSD
                             </div>
                           </dd>
                         </dl>
@@ -78,6 +148,61 @@ export const AddSwapLiquidity: React.FC = ({ match }) => {
                   </div>
                 </div>
 
+              </div>
+
+              <div className="mt-6 relative rounded-md shadow-sm">
+                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                  <span className="text-gray-500 sm:text-sm">
+                  </span>
+                </div>
+                <div className="absolute inset-y-0 right-0 flex items-center">
+                  <label htmlFor="tokenXAmount" className="sr-only">Token X</label>
+                  <select id="tokenX" name="tokenX"
+                      value={tokenX}
+                      className="focus:ring-indigo-500 focus:border-indigo-500 h-full py-0 pl-2 pr-7 border-transparent bg-transparent text-gray-500 sm:text-sm rounded-md">
+                    <option>{tokenX}</option>
+                  </select>
+                </div>
+                <input type="text" name="tokenXAmount" id="tokenXAmount"
+                  value={tokenXAmount}
+                  onChange={onInputChange}
+                  className="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md" placeholder="0.00" />
+              </div>
+              <p className="font-light ml-2">Balance: {balanceSelectedTokenX} {tokenX}</p>
+
+              <svg className="h-5 w-5 ml-8 mt-5 mb-5" x-description="Heroicon name: solid/chevron-down" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd"></path>
+              </svg>
+
+              <div className="mt-1 relative rounded-md shadow-sm">
+                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                  <span className="text-gray-500 sm:text-sm">
+                  </span>
+                </div>
+                <input type="text" name="tokenYAmount" id="tokenYAmount"
+                  value={tokenYAmount}
+                  disabled={true}
+                  className="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md" placeholder="0.00" />
+                <div className="absolute inset-y-0 right-0 flex items-center">
+                  <label htmlFor="tokenYAmount" className="sr-only">Token Y</label>
+                  <select id="tokenY" name="tokenY"
+                      value={tokenY}
+                      className="focus:ring-indigo-500 focus:border-indigo-500 h-full py-0 pl-2 pr-7 border-transparent bg-transparent text-gray-500 sm:text-sm rounded-md">
+                    <option>{tokenY}</option>
+                  </select>
+                </div>
+              </div>
+              <p className="font-light ml-2">Balance: {balanceSelectedTokenY} {tokenY}</p>
+              <p className="font-light ml-2">1 {tokenY} = ~{currentPrice} {tokenX}</p>
+
+              <div className="mt-5 ml-5 sm:flex sm:items-start sm:justify-between">
+                <div className="max-w-xl text-sm text-gray-500">
+                  <div className="mt-5 sm:mt-0 sm:flex-shrink-0 sm:flex sm:items-right">
+                    <button type="button" onClick={() => addLiquidity()} className="inline-flex items-right px-4 py-2 border border-transparent shadow-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm">
+                      Add Liquidity
+                    </button>
+                  </div>
+                </div>
               </div>
             </div>
           </main>
