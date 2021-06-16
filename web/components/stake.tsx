15d4aa90e3c222177931925eb9892e85c99891d5
Philip De Smedt <philip.desmedt@gmail.com>
2021-06-16 11:43:46 +0200

    fix typo

--- web/components/stake.tsx
+++ web/components/stake.tsx
@@ -345,7 +345,7 @@ export const Stake = () => {
                               scope="col"
                               className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                             >
-                              Stacked Value
+                              Staked Value
                             </th>
                             <th
                               scope="col"
