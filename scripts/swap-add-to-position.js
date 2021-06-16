a2bc5ce4a6e61e9689012ac1192a2d78220d474c
Philip De Smedt <philip.desmedt@gmail.com>
2021-06-16 15:35:22 +0200

    add script to add to position

---
+++ scripts/swap-add-to-position.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
+const CONTRACT_NAME = 'arkadiko-swap-v1-1';
+const FUNCTION_NAME = 'add-to-position';
+const tx = require('@stacks/transactions');
+const BN = require('bn.js');
+const utils = require('./utils');
+const network = utils.resolveNetwork();
+
+const add = async () => {
+  let nonce = await utils.getNonce(CONTRACT_ADDRESS);
+
+  const txOptions = {
+    contractAddress: CONTRACT_ADDRESS,
+    contractName: CONTRACT_NAME,
+    functionName: FUNCTION_NAME,
+    functionArgs: [
+      tx.contractPrincipalCV(CONTRACT_ADDRESS, 'wrapped-stx-token'),
+      tx.contractPrincipalCV(CONTRACT_ADDRESS, 'xusd-token'),
+      tx.contractPrincipalCV(CONTRACT_ADDRESS, 'arkadiko-swap-token-wstx-xusd'),
+      tx.uintCV(new BN(450000000000)), // 450000 wSTX
+      tx.uintCV(new BN(450000000000)) // 450000 xUSD
+    ],
+    senderKey: process.env.STACKS_PRIVATE_KEY,
+    postConditionMode: 1,
+    nonce: new BN(nonce),
+    network
+  };
+
+  const transaction = await tx.makeContractCall(txOptions);
+  const result = tx.broadcastTransaction(transaction, network);
+  await utils.processing(result, transaction.txid(), 0);
+};
+
+add();
