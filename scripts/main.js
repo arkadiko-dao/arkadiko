79a593207751d0d6ff1b66803f43ab972c7a2ae8
Philip De Smedt <philip.desmedt@gmail.com>
2021-06-17 11:27:04 +0200

    update script

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,23 @@
-function execute() {
-  console.log('1. run price oracle');
+function executePrice() {
+  console.log('- run price oracle');
   setTimeout(function() {
     require('child_process').fork('update-price.js');
   }, 0);
+}
 
-  console.log('2. end auctions that are running?');
-  console.log('3. end governance proposals that are running?');
-  console.log('4. liquidate vaults');
-
-  console.log('5. increase cumm rewards');
+function executeRewardCalibration() {
+  console.log('- increase cumm rewards');
   setTimeout(function() {
     require('child_process').fork('update-cumm-rewards.js');
   }, 2000);
 }
 
-console.log('Running price, auction and governance scripts');
-setInterval(execute, 60000 * 20); // every 20 minutes
+function executeAuctionLogic() {
+  console.log('2. end auctions that are running?');
+  console.log('3. end governance proposals that are running?');
+  console.log('4. liquidate vaults');
+}
+
+console.log('Running Arkadiko scripts');
+setInterval(executePrice, 60000 * 20); // every 20 mins
+setInterval(executeRewardCalibration, 60000 * 45); // every 45 mins
