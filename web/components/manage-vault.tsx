16967db25169da532ef04fce06b8e0b345f19e47
Maxime Laforet <me@macx.im>
2021-06-23 11:58:17 +0100

    Fix typo

--- web/components/manage-vault.tsx
+++ web/components/manage-vault.tsx
@@ -481,7 +481,7 @@ export const ManageVault = ({ match }) => {
                 className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:ml-3 sm:w-auto sm:text-sm"
                 onClick={() => addDeposit()}
               >
-                Mint
+                Add deposit
               </button>
               <button
                 type="button"
