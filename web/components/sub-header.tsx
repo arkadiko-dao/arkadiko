a9b2dcdee3225959d6b74d75fd89ca602106078e
Maxime Laforet <me@macx.im>
2021-06-25 11:16:10 +0100

    Update design

--- web/components/sub-header.tsx
+++ web/components/sub-header.tsx
@@ -6,30 +6,24 @@ export const SubHeader: React.FC = () => {
   const [state, _] = useContext(AppContext);
 
   return (
-    <div className="relative bg-indigo-500">
+    <div className="hidden sm:block relative bg-indigo-500">
       <div className="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
         <div className="pr-16 sm:text-center sm:px-16">
-          <p className="font-small text-white">
-            <span className="block sm:inline-block">
-              Balance: {microToReadable(state.balance['stx']).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 6 })} STX
-            </span>
-            <span className="block ml-2 mr-2 inline-block">
-              /
-            </span>
-            <span className="block inline-block">
-              {microToReadable(state.balance['xusd']).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 6 })} xUSD
-            </span>
-            <span className="block ml-2 mr-2 inline-block">
-              /
-            </span>
-            <span className="block inline-block">
-              {microToReadable(state.balance['diko']).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 6 })} DIKO
-            </span>
-            <span className="block ml-2 mr-2 inline-block">
-              /
-            </span>
-            <span className="block inline-block">
-              {microToReadable(state.balance['stdiko']).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 6 })} stDIKO
+          <p className="text-white">
+            <span className="text-xs font-semibold uppercase mr-6">Balance:</span>
+            <span className="space-y-1 sm:space-y-0 sm:space-x-6">
+              <span className="text-sm block sm:mt-0 sm:inline-block">
+                <span className="font-semibold">{microToReadable(state.balance['stx']).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 6 })}</span> <span className="opacity-75">STX</span>
+              </span>
+              <span className="text-sm block sm:mt-0 sm:inline-block">
+                <span className="font-semibold">{microToReadable(state.balance['xusd']).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 6 })}</span> <span className="opacity-75">xUSD</span>
+              </span>
+              <span className="text-sm block sm:mt-0 sm:inline-block">
+                <span className="font-semibold">{microToReadable(state.balance['diko']).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 6 })}</span> <span className="opacity-75">DIKO</span>
+              </span>
+              <span className="text-sm block sm:mt-0 sm:inline-block">
+                <span className="font-semibold">{microToReadable(state.balance['stdiko']).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 6 })}</span> <span className="opacity-75">stDIKO</span>
+              </span>
             </span>
           </p>
         </div>
