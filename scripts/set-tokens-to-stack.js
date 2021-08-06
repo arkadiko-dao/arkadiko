a02e32053562178949c71457acd4777e24c7edcd
Philip De Smedt <philip.desmedt@gmail.com>
2021-08-06 12:52:14 +0200

    fix pox settings

--- scripts/set-tokens-to-stack.js
+++ scripts/set-tokens-to-stack.js
@@ -4,22 +4,15 @@ const tx = require('@stacks/transactions');
 const utils = require('./utils');
 const network = utils.resolveNetwork();
 const BN = require('bn.js');
-const stacking = require('@stacks/stacking');
-const c32 = require('c32check');
 
 async function initiateStacking() {
-  const btcAddr = c32.c32ToB58('ST1QV6WVNED49CR34E58CRGA0V58X281FAS1TFBWF');
-  console.log(btcAddr);
-  const { hashMode, data } = stacking.decodeBtcAddress(btcAddr);
-  const hashbytes = tx.bufferCV(data);
   const txOptions = {
     contractAddress: CONTRACT_ADDRESS,
-    contractName: "arkadiko-stacker-v1-1",
-    functionName: "initiate-stacking",
+    contractName: "arkadiko-stx-reserve-v1-1",
+    functionName: "set-tokens-to-stack",
     functionArgs: [
-      tx.tupleCV({ 'version': tx.bufferCV(new BN(hashMode, 10).toArrayLike(Buffer)), 'hashbytes': hashbytes }),
-      tx.uintCV(149), // prepare_phase_start_block_height
-      tx.uintCV(3) // number of cycles
+      tx.stringAsciiCV('stacker'),
+      tx.uintCV(400000000000)
     ],
     senderKey: process.env.STACKS_PRIVATE_KEY,
     postConditionMode: 1,
