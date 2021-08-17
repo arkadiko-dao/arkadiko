ea8c36e2e8aef23f91f32a31c7a79f8813281097
Philip De Smedt <philip.desmedt@gmail.com>
2021-08-17 14:11:40 +0200

    fix

--- web/common/get-collateral-to-debt-ratio.ts
+++ web/common/get-collateral-to-debt-ratio.ts
@@ -2,7 +2,7 @@ import { useContext, useState, useEffect } from 'react';
 import { AppContext } from '@common/context';
 import { useSTXAddress } from './use-stx-address';
 import { stacksNetwork as network } from '@common/utils';
-import { callReadOnlyFunction, contractPrincipalCV, cvToJSON, uintCV } from '@stacks/transactions';
+import { callReadOnlyFunction, contractPrincipalCV, cvToJSON, uintCV, falseCV} from '@stacks/transactions';
 
 export const getCollateralToDebtRatio = (vaultId: string) => {
   const stxAddress = useSTXAddress();
@@ -11,25 +11,26 @@ export const getCollateralToDebtRatio = (vaultId: string) => {
   const contractAddress = process.env.REACT_APP_CONTRACT_ADDRESS || '';
 
   useEffect(() => {
-    // const getCollateralToDebtRatio = async () => {
-    //   const collToDebt = await callReadOnlyFunction({
-    //     contractAddress,
-    //     contractName: "arkadiko-freddie-v1-1",
-    //     functionName: "calculate-current-collateral-to-debt-ratio",
-    //     functionArgs: [
-    //       uintCV(vaultId),
-    //       contractPrincipalCV(contractAddress || '', 'arkadiko-collateral-types-v1-1'),
-    //       contractPrincipalCV(contractAddress || '', 'arkadiko-oracle-v1-1')
-    //     ],
-    //     senderAddress: stxAddress || '',
-    //     network: network
-    //   });
-    //   const json = cvToJSON(collToDebt);
-    //   if (json.value) {
-    //     setCollateralToDebt(json.value.value);
-    //   }
-    // };
-    // void getCollateralToDebtRatio();
+    const getCollateralToDebtRatio = async () => {
+      const collToDebt = await callReadOnlyFunction({
+        contractAddress,
+        contractName: "arkadiko-freddie-v1-1",
+        functionName: "calculate-current-collateral-to-debt-ratio",
+        functionArgs: [
+          uintCV(vaultId),
+          contractPrincipalCV(contractAddress || '', 'arkadiko-collateral-types-v1-1'),
+          contractPrincipalCV(contractAddress || '', 'arkadiko-oracle-v1-1'),
+          falseCV()
+        ],
+        senderAddress: stxAddress || '',
+        network: network
+      });
+      const json = cvToJSON(collToDebt);
+      if (json.value) {
+        setCollateralToDebt(json.value.value);
+      }
+    };
+    void getCollateralToDebtRatio();
   }, [state.userData]);
 
   return {
