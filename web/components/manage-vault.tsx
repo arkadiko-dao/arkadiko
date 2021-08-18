2b201e58b628149d48152b6bf5e327a54abea3c7
Maxime Laforet <me@macx.im>
2021-08-18 16:08:40 +0100

    Remove Switch

--- web/components/manage-vault.tsx
+++ web/components/manage-vault.tsx
@@ -1,8 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { Text, Modal, Tooltip } from '@blockstack/ui';
+import { Modal, Tooltip } from '@blockstack/ui';
 import { XIcon } from '@heroicons/react/outline';
 import { InformationCircleIcon } from '@heroicons/react/solid';
-import { Switch } from '@headlessui/react';
 import { Container } from './home';
 import { stacksNetwork as network } from '@common/utils';
 import { useSTXAddress } from '@common/use-stx-address';
@@ -22,9 +21,6 @@ import BN from 'bn.js';
 import { tokenList } from '@components/token-swap-list';
 import { InputAmount } from './input-amount';
 
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
-}
 
 export const ManageVault = ({ match }) => {
   const { doContractCall } = useConnect();
@@ -872,56 +868,8 @@ export const ManageVault = ({ match }) => {
                      <div className="pt-6">
                       <div>
                         <div className="flex items-start justify-between">
-                          <div>
-                            <p className="text-xl font-semibold leading-none">Stacking</p>
-                            <p className="text-base font-normal leading-6 text-green-500">Enabled</p>
-                          </div>
-
-                          <Switch
-                            checked={enabledStacking}
-                            onChange={setEnabledStacking}
-                            className={classNames(
-                              enabledStacking ? 'bg-indigo-600' : 'bg-gray-200',
-                              'relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
-                            )}
-                          >
-                            <span className="sr-only">Use setting</span>
-                            <span
-                              className={classNames(
-                                enabledStacking ? 'translate-x-5' : 'translate-x-0',
-                                'pointer-events-none relative inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200'
-                              )}
-                            >
-                              <span
-                                className={classNames(
-                                  enabledStacking ? 'opacity-0 ease-out duration-100' : 'opacity-100 ease-in duration-200',
-                                  'absolute inset-0 h-full w-full flex items-center justify-center transition-opacity'
-                                )}
-                                aria-hidden="true"
-                              >
-                                <svg className="w-3 h-3 text-gray-400" fill="none" viewBox="0 0 12 12">
-                                  <path
-                                    d="M4 8l2-2m0 0l2-2M6 6L4 4m2 2l2 2"
-                                    stroke="currentColor"
-                                    strokeWidth={2}
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                  />
-                                </svg>
-                              </span>
-                              <span
-                                className={classNames(
-                                  enabledStacking ? 'opacity-100 ease-in duration-200' : 'opacity-0 ease-out duration-100',
-                                  'absolute inset-0 h-full w-full flex items-center justify-center transition-opacity'
-                                )}
-                                aria-hidden="true"
-                              >
-                                <svg className="w-3 h-3 text-indigo-600" fill="currentColor" viewBox="0 0 12 12">
-                                  <path d="M3.707 5.293a1 1 0 00-1.414 1.414l1.414-1.414zM5 8l-.707.707a1 1 0 001.414 0L5 8zm4.707-3.293a1 1 0 00-1.414-1.414l1.414 1.414zm-7.414 2l2 2 1.414-1.414-2-2-1.414 1.414zm3.414 2l4-4-1.414-1.414-4 4 1.414 1.414z" />
-                                </svg>
-                              </span>
-                            </span>
-                          </Switch>
+                          <p className="text-xl font-semibold leading-none">Stacking</p>
+                          <p className="text-base font-normal leading-6 text-green-500">Enabled</p>
                         </div>
                       </div>
                       <div className="mt-4">
