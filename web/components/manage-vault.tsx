cdfa5b80bfbf09422a0d612b83511dfdd69ba7e1
Philip De Smedt <philip.desmedt@gmail.com>
2021-08-19 11:31:42 +0200

    fix front-end unlock collateral button

--- web/components/manage-vault.tsx
+++ web/components/manage-vault.tsx
@@ -869,7 +869,7 @@ export const ManageVault = ({ match }) => {
                             Withdraw
                           </button>
                         ) : null }
-                        {isVaultOwner && canUnlockCollateral ? (
+                        {isVaultOwner && canUnlockCollateral && vault?.stackedTokens > 0 ? (
                           <button 
                           type="button" 
                           className="inline-flex items-center px-3 py-2 text-sm font-medium leading-4 text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
