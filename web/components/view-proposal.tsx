e90b12cc33cdb7e508c65c59fc373797a4c696e0
Niel Deckx <contact@nieldeckx.be>
2021-08-31 12:49:26 +0200

    feat: add ability to vote with stDIKO from UI (#183)
    * Add ability to vote with stDIKO to UI
    
    * Added post conditions to voting.
    
    Co-authored-by: Niel Deckx <>
    Co-authored-by: Philip De Smedt <philip.desmedt@gmail.com>

--- web/components/view-proposal.tsx
+++ web/components/view-proposal.tsx
@@ -1,7 +1,10 @@
 import React, { useEffect, useState, useContext } from 'react';
 import { Modal } from '@blockstack/ui';
 import { Container } from './home';
-import { AnchorMode, callReadOnlyFunction, contractPrincipalCV, uintCV, cvToJSON } from '@stacks/transactions';
+import { 
+  AnchorMode, callReadOnlyFunction, contractPrincipalCV, createAssetInfo,
+  uintCV, cvToJSON, FungibleConditionCode, makeStandardFungiblePostCondition 
+} from '@stacks/transactions';
 import { stacksNetwork as network } from '@common/utils';
 import { useSTXAddress } from '@common/use-stx-address';
 import { useConnect } from '@stacks/connect-react';
@@ -10,13 +13,16 @@ import { ThumbUpIcon, ThumbDownIcon } from '@heroicons/react/outline';
 import { ExternalLinkIcon } from '@heroicons/react/solid';
 import { getRPCClient } from '@common/utils';
 import { ProposalProps } from './proposal-group';
+import BN from 'bn.js';
 
 export const ViewProposal = ({ match }) => {
   const [state, setState] = useContext(AppContext);
   const stxAddress = useSTXAddress();
   const [proposal, setProposal] = useState<ProposalProps>({});
-  const [showVoteModal, setShowVoteModal] = useState(false);
-  const [amountOfVotes, setAmountOfVotes] = useState('');
+  const [showVoteDikoModal, setShowVoteDikoModal] = useState(false);
+  const [showVoteStdikoModal, setShowVoteStdikoModal] = useState(false);
+  const [amountOfDikoVotes, setAmountOfDikoVotes] = useState('');
+  const [amountOfStdikoVotes, setAmountOfStdikoVotes] = useState('');
   const { doContractCall } = useConnect();
   const contractAddress = process.env.REACT_APP_CONTRACT_ADDRESS || '';
   const [stacksTipHeight, setStacksTipHeight] = useState(0);
@@ -71,7 +77,19 @@ export const ViewProposal = ({ match }) => {
     return () => { mounted = false; }
   }, []);
 
-  const addVoteFor = async () => {
+  const addVoteDikoFor = async () => {
+    const postConditions = [
+      makeStandardFungiblePostCondition(
+        stxAddress || '',
+        FungibleConditionCode.Equal,
+        new BN(amountOfDikoVotes * 1000000),
+        createAssetInfo(
+          contractAddress,
+          "arkadiko-token",
+          "diko"
+        )
+      )
+    ];
     await doContractCall({
       network,
       contractAddress,
@@ -81,18 +99,31 @@ export const ViewProposal = ({ match }) => {
       functionArgs: [
         contractPrincipalCV(process.env.REACT_APP_CONTRACT_ADDRESS || '', 'arkadiko-stake-pool-diko-v1-1'),
         contractPrincipalCV(process.env.REACT_APP_CONTRACT_ADDRESS || '', 'arkadiko-token'),
-        uintCV(match.params.id), uintCV(amountOfVotes * 1000000)
+        uintCV(match.params.id), uintCV(amountOfDikoVotes * 1000000)
       ],
       postConditionMode: 0x01,
+      postConditions,
       finished: data => {
         setState(prevState => ({ ...prevState, currentTxId: data.txId, currentTxStatus: 'pending' }));
-        setShowVoteModal(false);
+        setShowVoteDikoModal(false);
       },
       anchorMode: AnchorMode.Any
     });
   };
 
-  const addVoteAgainst = async () => {
+  const addVoteDikoAgainst = async () => {
+    const postConditions = [
+      makeStandardFungiblePostCondition(
+        stxAddress || '',
+        FungibleConditionCode.Equal,
+        new BN(amountOfDikoVotes * 1000000),
+        createAssetInfo(
+          contractAddress,
+          "arkadiko-token",
+          "diko"
+        )
+      )
+    ];
     await doContractCall({
       network,
       contractAddress,
@@ -102,25 +133,99 @@ export const ViewProposal = ({ match }) => {
       functionArgs: [
         contractPrincipalCV(process.env.REACT_APP_CONTRACT_ADDRESS || '', 'arkadiko-stake-pool-diko-v1-1'),
         contractPrincipalCV(process.env.REACT_APP_CONTRACT_ADDRESS || '', 'arkadiko-token'),
-        uintCV(match.params.id), uintCV(amountOfVotes * 1000000)
+        uintCV(match.params.id), uintCV(amountOfDikoVotes * 1000000)
       ],
       postConditionMode: 0x01,
+      postConditions,
       finished: data => {
         setState(prevState => ({ ...prevState, currentTxId: data.txId, currentTxStatus: 'pending' }));
-        setShowVoteModal(false);
+        setShowVoteDikoModal(false);
       },
       anchorMode: AnchorMode.Any
     });
   };
 
-  const onInputChange = (event) => {
+  const onInputDikoChange = (event) => {
     const value = event.target.value;
-    setAmountOfVotes(value);
+    setAmountOfDikoVotes(value);
+  };
+
+  const addVoteStdikoFor = async () => {
+    const postConditions = [
+      makeStandardFungiblePostCondition(
+        stxAddress || '',
+        FungibleConditionCode.Equal,
+        new BN(amountOfStdikoVotes * 1000000),
+        createAssetInfo(
+          contractAddress,
+          "stdiko-token",
+          "stdiko"
+        )
+      )
+    ];
+    await doContractCall({
+      network,
+      contractAddress,
+      stxAddress,
+      contractName: 'arkadiko-governance-v1-1',
+      functionName: 'vote-for',
+      functionArgs: [
+        contractPrincipalCV(process.env.REACT_APP_CONTRACT_ADDRESS || '', 'arkadiko-stake-pool-diko-v1-1'),
+        contractPrincipalCV(process.env.REACT_APP_CONTRACT_ADDRESS || '', 'stdiko-token'),
+        uintCV(match.params.id), uintCV(amountOfStdikoVotes * 1000000)
+      ],
+      postConditionMode: 0x01,
+      postConditions,
+      finished: data => {
+        setState(prevState => ({ ...prevState, currentTxId: data.txId, currentTxStatus: 'pending' }));
+        setShowVoteStdikoModal(false);
+      },
+      anchorMode: AnchorMode.Any
+    });
+  };
+
+  const addVoteStdikoAgainst = async () => {
+    const postConditions = [
+      makeStandardFungiblePostCondition(
+        stxAddress || '',
+        FungibleConditionCode.Equal,
+        new BN(amountOfStdikoVotes * 1000000),
+        createAssetInfo(
+          contractAddress,
+          "stdiko-token",
+          "stdiko"
+        )
+      )
+    ];
+    await doContractCall({
+      network,
+      contractAddress,
+      stxAddress,
+      contractName: 'arkadiko-governance-v1-1',
+      functionName: 'vote-against',
+      functionArgs: [
+        contractPrincipalCV(process.env.REACT_APP_CONTRACT_ADDRESS || '', 'arkadiko-stake-pool-diko-v1-1'),
+        contractPrincipalCV(process.env.REACT_APP_CONTRACT_ADDRESS || '', 'stdiko-token'),
+        uintCV(match.params.id), uintCV(amountOfStdikoVotes * 1000000)
+      ],
+      postConditionMode: 0x01,
+      postConditions,
+      finished: data => {
+        setState(prevState => ({ ...prevState, currentTxId: data.txId, currentTxStatus: 'pending' }));
+        setShowVoteStdikoModal(false);
+      },
+      anchorMode: AnchorMode.Any
+    });
+  };
+
+  const onInputStdikoChange = (event) => {
+    const value = event.target.value;
+    setAmountOfStdikoVotes(value);
   };
 
   return (
     <Container>
-      <Modal isOpen={showVoteModal}>
+      <Modal isOpen={showVoteDikoModal}>
         <div className="flex px-4 pt-4 pb-20 text-center sm:block sm:p-0">
           <div className="inline-block px-2 pt-5 pb-4 overflow-hidden text-left align-bottom bg-white rounded-lg sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
             <div>
@@ -142,8 +247,8 @@ export const ViewProposal = ({ match }) => {
                     <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                     </div>
                     <input type="text" name="stx" id="stxAmount"
-                          value={amountOfVotes}
-                          onChange={onInputChange}
+                          value={amountOfDikoVotes}
+                          onChange={onInputDikoChange}
                           className="block w-full pr-12 border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500 pl-7 sm:text-sm"
                           placeholder="0.00" aria-describedby="stx-currency" />
                     <div className="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
@@ -157,15 +262,68 @@ export const ViewProposal = ({ match }) => {
               </div>
             </div>
             <div className="mt-5 sm:mt-6">
-              <button type="button" onClick={() => addVoteFor()} className="inline-flex justify-center w-full px-4 py-2 mb-5 text-base font-medium text-white bg-green-600 border border-transparent rounded-md shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm">
+              <button type="button" onClick={() => addVoteDikoFor()} className="inline-flex justify-center w-full px-4 py-2 mb-5 text-base font-medium text-white bg-green-600 border border-transparent rounded-md shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm">
                 Vote For
               </button>
 
-              <button type="button" onClick={() => addVoteAgainst()} className="inline-flex justify-center w-full px-4 py-2 mb-5 text-base font-medium text-white bg-red-600 border border-transparent rounded-md shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 sm:text-sm">
+              <button type="button" onClick={() => addVoteDikoAgainst()} className="inline-flex justify-center w-full px-4 py-2 mb-5 text-base font-medium text-white bg-red-600 border border-transparent rounded-md shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 sm:text-sm">
                 Vote Against
               </button>
 
-              <button type="button" onClick={() => setShowVoteModal(false)} className="inline-flex justify-center w-full px-4 py-2 text-base font-medium text-white bg-gray-600 border border-transparent rounded-md shadow-sm hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 sm:text-sm">
+              <button type="button" onClick={() => setShowVoteDikoModal(false)} className="inline-flex justify-center w-full px-4 py-2 text-base font-medium text-white bg-gray-600 border border-transparent rounded-md shadow-sm hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 sm:text-sm">
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      </Modal>
+
+      <Modal isOpen={showVoteStdikoModal}>
+        <div className="flex px-4 pt-4 pb-20 text-center sm:block sm:p-0">
+          <div className="inline-block px-2 pt-5 pb-4 overflow-hidden text-left align-bottom bg-white rounded-lg sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
+            <div>
+              <div className="flex items-center justify-center w-12 h-12 mx-auto bg-green-100 rounded-full">
+                <svg className="w-6 h-6 text-green-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" />
+                </svg>
+              </div>
+              <div className="mt-3 text-center sm:mt-5">
+                <h3 className="text-lg font-medium leading-6 text-gray-900 font-headings" id="modal-headline">
+                  Vote for Proposal {match.params.id}
+                </h3>
+                <div className="mt-2">
+                  <p className="text-sm text-gray-500">
+                    {proposal.title}
+                  </p>
+
+                  <div className="relative mt-4 rounded-md shadow-sm">
+                    <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                    </div>
+                    <input type="text" name="stx" id="stxAmount"
+                          value={amountOfStdikoVotes}
+                          onChange={onInputStdikoChange}
+                          className="block w-full pr-12 border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500 pl-7 sm:text-sm"
+                          placeholder="0.00" aria-describedby="stx-currency" />
+                    <div className="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
+                      <span className="text-gray-500 sm:text-sm" id="stx-currency">
+                        stDIKO
+                      </span>
+                    </div>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+            <div className="mt-5 sm:mt-6">
+              <button type="button" onClick={() => addVoteStdikoFor()} className="inline-flex justify-center w-full px-4 py-2 mb-5 text-base font-medium text-white bg-green-600 border border-transparent rounded-md shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm">
+                Vote For
+              </button>
+
+              <button type="button" onClick={() => addVoteStdikoAgainst()} className="inline-flex justify-center w-full px-4 py-2 mb-5 text-base font-medium text-white bg-red-600 border border-transparent rounded-md shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 sm:text-sm">
+                Vote Against
+              </button>
+
+              <button type="button" onClick={() => setShowVoteStdikoModal(false)} className="inline-flex justify-center w-full px-4 py-2 text-base font-medium text-white bg-gray-600 border border-transparent rounded-md shadow-sm hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 sm:text-sm">
                 Close
               </button>
             </div>
@@ -241,21 +399,33 @@ export const ViewProposal = ({ match }) => {
                     <ThumbUpIcon className="block w-6 h-6 mr-2 text-gray-400" aria-hidden="true" />
                     Vote For
                   </dt>
-                  <dd className="order-1 text-3xl font-bold text-indigo-600">{proposal.forVotes / 1000000} DIKO</dd>
+                  <dd className="order-1 text-3xl font-bold text-indigo-600">{proposal.forVotes / 1000000}</dd>
                 </div>
                 <div className="flex flex-col justify-center p-6 text-center border-t border-gray-100 sm:border-0 sm:border-l">
                   <dt className="inline-flex items-center order-2 mx-auto mt-2 text-lg font-medium leading-6 text-gray-500">
                     <ThumbDownIcon className="block w-6 h-6 mr-2 text-gray-400" aria-hidden="true" />
                     Vote Against
                   </dt>
-                  <dd className="order-1 text-3xl font-bold text-indigo-600">{proposal.against / 1000000} DIKO</dd>
+                  <dd className="order-1 text-3xl font-bold text-indigo-600">{proposal.against / 1000000}</dd>
                 </div>
               </dl>
             </div>
             <div className="mt-6">
               <div className="flex justify-center">
-                <button type="button" onClick={() => setShowVoteModal(true)} className="inline-flex items-center px-6 py-3 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                  Vote
+                <button type="button" onClick={() => setShowVoteDikoModal(true)} className="inline-flex items-center px-6 py-3 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                  Vote with DIKO
+                </button>
+              </div>
+            </div>
+            <div className="mt-0">
+              <div className="flex justify-center">
+                - or -
+              </div>
+            </div>
+            <div className="mt-0">
+              <div className="flex justify-center">
+                <button type="button" onClick={() => setShowVoteStdikoModal(true)} className="inline-flex items-center px-6 py-3 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                  Vote with stDIKO
                 </button>
               </div>
             </div>
