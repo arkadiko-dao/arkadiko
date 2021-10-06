b566cdce49ebd23c2ba9f72a957879c88783dcfb
Maxime Laforet <me@macx.im>
2021-10-06 13:09:13 +0100

    Add info about unstaked LP token in user's wallet
    Add borders on all alerts for consistency
    
    add logic to check on unstaked LP tokens

--- web/components/create-vault-transact.tsx
+++ web/components/create-vault-transact.tsx
@@ -84,7 +84,7 @@ export const CreateVaultTransact = ({ coinAmounts }) => {
   return (
     <div className="max-w-4xl mx-auto">
       {state.currentTxId ? (
-        <div className="p-4 rounded-md bg-green-50">
+        <div className="p-4 border-l-4 border-green-400 rounded-tr-md rounded-br-md bg-green-50">
           <div className="flex">
             <div className="flex-shrink-0">
               <CheckCircleIcon className="w-5 h-5 text-green-400" aria-hidden="true" />
@@ -104,13 +104,13 @@ export const CreateVaultTransact = ({ coinAmounts }) => {
           </div>
         </div>
       ) : (
-        <div className="p-4 rounded-md bg-yellow-50">
+        <div className="p-4 border-l-4 border-yellow-400 rounded-tr-md rounded-br-md bg-yellow-50">
           <div className="flex">
             <div className="flex-shrink-0">
               <ExclamationIcon className="w-5 h-5 text-yellow-400" aria-hidden="true" />
             </div>
             <div className="ml-3">
-              <h3 className="text-sm font-medium text-yellow-800">Attention needed</h3>
+              <h3 className="text-sm font-semibold text-yellow-800">Attention needed</h3>
               <div className="mt-2 text-sm text-yellow-700">
                 <p>
                   Confirm the transaction to create your new vault
