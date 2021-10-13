55ff636f48a5b7b65c836eeaf460bc7f75406989
Maxime Laforet <me@macx.im>
2021-10-13 10:16:57 +0100

    Fix TxSidebar component and children

--- web/components/contract-transaction.tsx
+++ web/components/contract-transaction.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ContractCallTransaction, MempoolContractCallTransaction } from '@blockstack/stacks-blockchain-api-types';
+import { classNames } from '@common/class-names';
 
 type ComponentProps = {
   transaction: ContractCallTransaction | MempoolContractCallTransaction,
@@ -14,23 +15,20 @@ export const ContractTransaction: React.FC<ComponentProps> = ({ transaction, sta
 
   return (
     <li className="py-4">
-      <div className="flex space-x-3">
-        {status === 'success' ? (
-          <span className="mt-3 space-y-1 w-1.5 h-1.5 bg-green-400 rounded-full"></span>
-        ) : status === 'error' ? (
-          <span className="mt-3 space-y-1 w-1.5 h-1.5 bg-red-600 rounded-full"></span>
-        ) : (
-          <span className="mt-3 space-y-1 w-1.5 h-1.5 bg-yellow-300 rounded-full"></span>
-        )}
-        <div className="flex-1 space-y-1">
-          <div className="flex items-center justify-between">
-            <h3 className="font-medium text-medium font-headings">
-              <a href={url} target="_blank">{transaction.contract_call.function_name}</a>
+      <a href={url} target="_blank">
+        <div className="flex items-baseline space-x-3">
+          <span className={classNames(
+            status === 'success' ? 'bg-green-400' : status === 'error' ? 'bg-red-600' : 'bg-yellow-300',
+            'mt-3 space-y-1 w-1.5 h-1.5 rounded-full'
+          )}/>
+          <div className="flex-1 space-y-1">
+            <h3 className="font-headings">
+              {transaction.contract_call.function_name}
             </h3>
+            <p className="text-sm text-gray-500">{contract_id}</p>
           </div>
-          <p className="text-sm text-gray-500">{contract_id}</p>
         </div>
-      </div>
+      </a>
     </li>
   );
 };
