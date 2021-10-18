8c3917b751f28c5e3dc20d3ad7d383ce0c0f4832
Philip De Smedt <philip.desmedt@gmail.com>
2021-10-18 10:02:52 +0200

    tweak: add some useful help scripts

---
+++ scripts/return-stx-to-stx-reserve.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
+const CONTRACT_NAME = 'arkadiko-stacker-v1-1';
+const FUNCTION_NAME = 'return-stx';
+const tx = require('@stacks/transactions');
+const utils = require('./utils');
+const network = utils.resolveNetwork();
+
+const txOptions = {
+  contractAddress: CONTRACT_ADDRESS,
+  contractName: CONTRACT_NAME,
+  functionName: FUNCTION_NAME,
+  functionArgs: [
+    tx.uintCV(50000000000),
+  ],
+  senderKey: process.env.STACKS_PRIVATE_KEY,
+  postConditionMode: 1,
+  network
+};
+
+async function transact() {
+  const transaction = await tx.makeContractCall(txOptions);
+  const result = tx.broadcastTransaction(transaction, network);
+  await utils.processing(result, transaction.txid(), 0);
+};
+
+transact();
