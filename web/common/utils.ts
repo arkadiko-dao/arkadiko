4d4721d2ec76d4daff1ce2efe2933e4878dc9c55
Niel Deckx <>
2021-10-20 09:22:17 +0200

    Remove unused date lib

--- web/common/utils.ts
+++ web/common/utils.ts
@@ -1,9 +1,6 @@
 import { RPCClient } from '@stacks/rpc-client';
-import dayjs from 'dayjs';
-import relativeTime from 'dayjs/plugin/relativeTime';
 import { StacksMainnet, StacksTestnet } from '@stacks/network';
 
-dayjs.extend(relativeTime);
 const env = process.env.REACT_APP_NETWORK_ENV || 'testnet';
 
 let coreApiUrl = 'https://stacks-node-api.mainnet.stacks.co';
@@ -19,8 +16,6 @@ if (env.includes('mocknet')) {
 export const getRPCClient = () => {
   return new RPCClient(coreApiUrl);
 };
-
-export const toRelativeTime = (ts: number): string => dayjs().to(ts);
 
 export const stacksNetwork = (env === 'mainnet') ? new StacksMainnet() : new StacksTestnet();
 stacksNetwork.coreApiUrl = coreApiUrl;
