f3d4429bcf9949abc58b9b2cdec392a23f6d2c16
Philip De Smedt <philip.desmedt@gmail.com>
2021-10-21 11:54:42 +0200

    Launch swap on block 34640

--- web/components/swap.tsx
+++ web/components/swap.tsx
@@ -430,7 +430,7 @@ export const Swap: React.FC = () => {
                       Hold on!
                     </h1>
                     <p className="mt-3 text-base text-gray-300">
-                      Arkadiko Swap will open up at block 34650 to give people time to post their liquidity.
+                      Arkadiko Swap will open up at block 34640 to give people time to post their liquidity.
                       Add your liquidity by clicking on Pool above.
                     </p>
                   </div>
