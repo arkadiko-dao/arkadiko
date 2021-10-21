09cc242986521f5ccf4d9da8d9eb9dfef0dccc59
Philip De Smedt <philip.desmedt@gmail.com>
2021-10-21 10:58:59 +0200

    hide xBTC for now

--- web/components/app.tsx
+++ web/components/app.tsx
@@ -86,7 +86,7 @@ export const App: React.FC = () => {
 
   const fetchCollateralTypes = async (address: string) => {
     let collTypes = {};
-    ['STX-A', 'STX-B', 'XBTC-A'].forEach(async (token) => {
+    ['STX-A', 'STX-B'].forEach(async (token) => {
       const types = await callReadOnlyFunction({
         contractAddress,
         contractName: "arkadiko-collateral-types-v1-1",
