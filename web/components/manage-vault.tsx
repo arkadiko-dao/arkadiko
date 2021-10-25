449b5d32dd37e8c746841dbfe356f85b39c17c66
Philip De Smedt <philip.desmedt@gmail.com>
2021-10-25 09:25:17 +0200

    fix: only show partial withdraw collateral button when debt > 0

--- web/components/manage-vault.tsx
+++ web/components/manage-vault.tsx
@@ -869,7 +869,7 @@ export const ManageVault = ({ match }) => {
                         <p className="text-base font-normal leading-6 text-gray-500">Able to withdraw</p>
                       </div>
 
-                      {isVaultOwner && canWithdrawCollateral ? (
+                      {isVaultOwner && canWithdrawCollateral && totalDebt > 0 ? (
                         <button 
                           type="button" 
                           className="inline-flex items-center px-3 py-2 text-sm font-medium leading-4 text-indigo-700 bg-indigo-100 border border-transparent rounded-md hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
