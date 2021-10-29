5d5c9b88bbfd2c7dc1fb6623cab1faedbe05d0be
Philip De Smedt <philip.desmedt@gmail.com>
2021-10-29 20:20:56 +0200

    add proposal to change swap in governance

---
+++ scripts/proposal-change-swap.js
@@ -0,0 +1,40 @@
+// node proposal-change-stacker.js
+require('dotenv').config();
+const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
+const tx = require('@stacks/transactions');
+const utils = require('./utils');
+const network = utils.resolveNetwork();
+
+async function transact() {
+  const list = tx.listCV([
+    tx.tupleCV({
+      'name': tx.stringAsciiCV("swap"),
+      'address': tx.standardPrincipalCV("SP2C2YFP12AJZB4MABJBAJ55XECVS7E4PMMZ89YZR"),
+      'qualified-name': tx.standardPrincipalCV("SP2C2YFP12AJZB4MABJBAJ55XECVS7E4PMMZ89YZR", "arkadiko-swap-v2-1"),
+      'can-mint': tx.trueCV(),
+      'can-burn': tx.trueCV()
+    })
+  ]);
+
+  const txOptions = {
+    contractAddress: CONTRACT_ADDRESS,
+    contractName: 'arkadiko-governance-v1-1',
+    functionName: 'propose',
+    functionArgs: [
+      tx.contractPrincipalCV(CONTRACT_ADDRESS, 'arkadiko-stake-pool-diko-v1-1'),
+      tx.uintCV(200),
+      tx.stringUtf8CV('Upgrade Arkadiko Swap Contract'),
+      tx.stringUtf8CV('https://github.com/arkadiko-dao/arkadiko/pull/316'),
+      list
+    ],
+    senderKey: process.env.STACKS_PRIVATE_KEY,
+    postConditionMode: 1,
+    network
+  };
+
+  const transaction = await tx.makeContractCall(txOptions);
+  const result = tx.broadcastTransaction(transaction, network);
+  await utils.processing(result, transaction.txid(), 0);
+};
+
+transact();
