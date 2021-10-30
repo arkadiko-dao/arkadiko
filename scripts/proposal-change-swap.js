83e99998d0dcc5738b5e18afb68a685737ce6904
Philip De Smedt <philip.desmedt@gmail.com>
2021-10-30 11:50:55 +0200

    add script to send STX

--- scripts/proposal-change-swap.js
+++ scripts/proposal-change-swap.js
@@ -6,26 +6,24 @@ const utils = require('./utils');
 const network = utils.resolveNetwork();
 
 async function transact() {
-  const list = tx.listCV([
-    tx.tupleCV({
-      'name': tx.stringAsciiCV("swap"),
-      'address': tx.standardPrincipalCV("SP2C2YFP12AJZB4MABJBAJ55XECVS7E4PMMZ89YZR"),
-      'qualified-name': tx.contractPrincipalCV("SP2C2YFP12AJZB4MABJBAJ55XECVS7E4PMMZ89YZR", "arkadiko-swap-v2-1"),
-      'can-mint': tx.trueCV(),
-      'can-burn': tx.trueCV()
-    })
-  ]);
-
   const txOptions = {
     contractAddress: CONTRACT_ADDRESS,
     contractName: 'arkadiko-governance-v1-1',
     functionName: 'propose',
     functionArgs: [
       tx.contractPrincipalCV(CONTRACT_ADDRESS, 'arkadiko-stake-pool-diko-v1-1'),
-      tx.uintCV(35835),
+      tx.uintCV(35840),
       tx.stringUtf8CV('Upgrade Arkadiko Swap Contract'),
       tx.stringUtf8CV('https://github.com/arkadiko-dao/arkadiko/pull/316'),
-      list
+      tx.listCV([
+        tx.tupleCV({
+          'name': tx.stringAsciiCV("swap"),
+          'address': tx.standardPrincipalCV("SP2C2YFP12AJZB4MABJBAJ55XECVS7E4PMMZ89YZR"),
+          'qualified-name': tx.contractPrincipalCV("SP2C2YFP12AJZB4MABJBAJ55XECVS7E4PMMZ89YZR", "arkadiko-swap-v2-1"),
+          'can-mint': tx.trueCV(),
+          'can-burn': tx.trueCV()
+        })
+      ])
     ],
     senderKey: process.env.STACKS_PRIVATE_KEY,
     postConditionMode: 1,
