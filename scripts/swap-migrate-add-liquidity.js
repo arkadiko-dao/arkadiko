1546133ad1e71d09326cfd4b8aa7605f03690f44
Philip De Smedt <philip.desmedt@gmail.com>
2021-11-05 11:02:29 +0100

    add migration scripts

---
+++ scripts/swap-migrate-add-liquidity.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
+const tx = require('@stacks/transactions');
+const utils = require('./utils');
+const network = utils.resolveNetwork();
+const BN = require('bn.js');
+
+const txOptions = {
+  contractAddress: CONTRACT_ADDRESS,
+  contractName: 'arkadiko-swap-v1-1',
+  functionName: 'migrate-add-liquidity',
+  functionArgs: [
+    tx.contractPrincipalCV(CONTRACT_ADDRESS, 'wrapped-stx-token'),
+    tx.contractPrincipalCV(CONTRACT_ADDRESS, 'arkadiko-token'),
+    tx.uintCV(100000),
+    tx.uintCV(100000)
+  ],
+  senderKey: process.env.STACKS_PRIVATE_KEY,
+  postConditionMode: 1,
+  network
+};
+
+async function transact() {
+  const transaction = await tx.makeContractCall(txOptions);
+  const result = tx.broadcastTransaction(transaction, network);
+  await utils.processing(result, transaction.txid(), 0);
+};
+
+transact();
