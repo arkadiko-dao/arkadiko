21473d7fe6bc43a5a73d699b7e9207f5a5acfe3e
Philip De Smedt <philip.desmedt@gmail.com>
2021-11-17 11:29:32 +0100

    feat: add contract for users to claim yields from PoX (#319)
    * feat: set up mainnet
    
    * set block heights for mainnet
    
    * add mainnet PoX address
    
    * add mainnet address for pools and DAO
    
    * add SIP 10 mainnet address
    
    * cleanup
    
    * set guardian start block to 35300
    
    * set correct order of deployment
    
    * add script to change oracle owner
    
    * do not show mainnet STX faucet
    
    * small fixes
    
    * initial liquidity: DIKO-USDA
    
    * higher fee for DIKO-USDA create pair TX
    
    * use .24 as exchange rate for STXUSD
    
    * fix exchange rate for STX/DIKO
    
    * link favicon
    
    * fix rounding issue
    
    * fix rounding error
    
    * redirect to pool by default
    
    * remove post conditions
    
    * Fix link to mainnet
    
    * add loading text on stake page
    
    * show loading placeholder when loading
    
    * show insufficient balance message if not enough balance to post liquidity
    
    * some fixes
    
    * hide xBTC for now
    
    * add swap message
    
    * hide pool tab
    
    * fix core API URL
    
    * fix 0 values in pool tab
    
    * Launch swap on block 34640
    
    * set default slippage at 2%
    
    * fix copy
    
    * use stacks node api url
    
    * fix broken vault page
    
    * fix max coins to mint on manage vault page
    
    * update message
    
    * set loading price placeholder
    
    * set slippage to 4%
    
    * fix: add loading state to create vault
    
    * use syvita endpoint
    
    * enable swap and add loading state
    
    * no postconditions
    
    * fix loading
    
    * fix loading
    
    * link TX to mainnet from sidebar
    
    * run PCs for removing swap liquidity on allow mode
    
    * fix: burning of USDA should include stability fee + add close vault button (#287)
    
    * fix: burning of USDA
    
    * fix
    
    * fix: leave some USDA in vault to allow STX withdrawal
    
    * fix
    
    * fix debt to pay subtraction
    
    * add option to close vault
    
    * Fix bug
    
    * add post condition mode for close vault
    
    * set allow mode on post conditions
    
    * tweak: add nicer Swap loading placeholder (#297)
    
    * tweak: add nicer Swap loading placeholder
    
    * Add tabs title + selected pairs
    
    * fix to avoid selecting same token in swap
    
    Co-authored-by: Niel Deckx <>
    
    * fix mint max button
    
    * fix: only show partial withdraw collateral button when debt > 0
    
    * fix: disable switching pairs in add liquidity screen (#309)
    
    * add real USDA price from oracle
    
    * fix APY -> APR
    
    * feat: update UX & design of manage vault (#279)
    
    Add tooltip explaining Available to mint
    
    Updated tooltip
    
    Add end of stacking date from #277
    
    [wip] add loading states
    
    different vars to keep loading state
    
    Tweaks
    
    Fixed price issue
    
    [wip] moar loading states
    
    Add more placeholders
    
    Remove extra margin
    
    Co-authored-by: Philip De Smedt <philip.desmedt@gmail.com>
    
    * wip: fixing mainnet branch
    
    * wip: fixing mainnet branch
    
    * wip: fixing mainnet branch
    
    * wip: fixing mainnet branch
    
    * remove swap temporarily
    
    * feat: add announcement banner (#317)
    
    * Add banner
    
    * Change icon color
    
    * add script to set pool data
    
    * Do not allow staking LP until swap fixed
    
    * ignore api/ folder for now
    
    * feat: add contract for users to claim yields from PoX
    
    * rename fn
    
    * rename fn
    
    * git status
    
    * Make sure STX is transfered to contract when adding claim.
    
    * Added tests
    
    * can now use yield to pay back vault stability fee and debt
    
    * fixed issue where user could claim multiple times
    
    * extra tests
    
    * Updated comments
    
    * Update deploy script
    
    * Script to add yield for a vault
    
    * Remove option to automatically pay back debt in confirmation screen
    
    * Added yield info and buttons to vault page
    
    * fix test
    
    * Fix buttons alignment
    
    * Test to check how we can add extra STX to stack
    
    * Claim can be stacked immediately
    
    * change comment
    
    * do not auto-stack
    
    * feat: remove claims
    
    Co-authored-by: Maxime Laforet <me@macx.im>
    Co-authored-by: Niel Deckx <>

---
+++ scripts/add-vault-yield.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
+const tx = require('@stacks/transactions');
+const BN = require('bn.js');
+const utils = require('./utils');
+const network = utils.resolveNetwork();
+
+async function addPoxYield() {
+  const txOptions = {
+    contractAddress: CONTRACT_ADDRESS,
+    contractName: "arkadiko-claim-yield-v1-1",
+    functionName: "add-claim",
+    functionArgs: [
+      tx.tupleCV({
+        'to': tx.uintCV(1),
+        'ustx': tx.uintCV(100000000)
+      })
+    ],
+    senderKey: process.env.STACKS_PRIVATE_KEY,
+    fee: new BN(5000000, 1),
+    postConditionMode: 1,
+    network
+  };
+
+  const transaction = await tx.makeContractCall(txOptions);
+  const result = tx.broadcastTransaction(transaction, network);
+  await utils.processing(result, transaction.txid(), 0);
+}
+
+addPoxYield();
