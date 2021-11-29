bf3da6d7726be3f3ee3a86b5815809da4336fdc1
Maxime Laforet <me@macx.im>
2021-11-29 12:50:00 +0000

    Extract sidebar

--- analytics-web/components/app.tsx
+++ analytics-web/components/app.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { ThemeProvider, theme } from '@blockstack/ui';
+import { Sidebar } from './sidebar';
 import { Routes } from '@components/routes';
 
 export const App: React.FC = () => {
   return (
     <ThemeProvider theme={theme}>
       <div className="flex flex-col h-full font-sans">
-        <Routes />
+        <Sidebar>
+          <Routes />
+        </Sidebar>
       </div>
     </ThemeProvider>
   );
