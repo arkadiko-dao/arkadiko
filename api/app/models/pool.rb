b4a6b5585dbd7cbc9fec0a04a775f735064304fe
Philip De Smedt <philip.desmedt@gmail.com>
2021-12-08 11:14:46 +0100

    tweak: fix unix timestamp format

--- api/app/models/pool.rb
+++ api/app/models/pool.rb
@@ -38,7 +38,7 @@ class Pool < ApplicationRecord
   def fetch_prices
     events = swap_events.where("function_name IN (?)", ['swap-x-for-y', 'swap-y-for-x'])
     # TODO: fix performance
-    events.map{|event| [event['event_at'].to_i, (event['token_y_amount'].to_f / event['token_x_amount'].to_f).round(2)]}
+    events.map{|event| [event['event_at'].to_i * 1000, (event['token_y_amount'].to_f / event['token_x_amount'].to_f).round(2)]}
   end
 
   def tvl
