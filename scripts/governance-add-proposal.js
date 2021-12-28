56d3af9422de354fbef6c19eeb80ae4b5a4b534c
philipdesmedt <philip.desmedt@gmail.com>
2021-12-28 10:40:32 +0100

    add welsh proposal

--- scripts/governance-add-proposal.js
+++ scripts/governance-add-proposal.js
@@ -16,12 +16,12 @@ async function transact() {
       tx.uintCV(43100),
       tx.uintCV(900),
       tx.stringUtf8CV('Add STX/WELSH Pair'),
-      tx.stringUtf8CV('https://github.com/arkadiko-dao/arkadiko/pull/399'),
+      tx.stringUtf8CV('https://github.com/arkadiko-dao/arkadiko/pull/408'),
       tx.listCV([
         tx.tupleCV({
-          'name': tx.stringAsciiCV("add-xbtc"),
+          'name': tx.stringAsciiCV("add-welsh"),
           'address': tx.standardPrincipalCV("SP2C2YFP12AJZB4MABJBAJ55XECVS7E4PMMZ89YZR"),
-          'qualified-name': tx.contractPrincipalCV("SP2C2YFP12AJZB4MABJBAJ55XECVS7E4PMMZ89YZR", "arkadiko-add-xbtc"),
+          'qualified-name': tx.contractPrincipalCV("SP2C2YFP12AJZB4MABJBAJ55XECVS7E4PMMZ89YZR", "arkadiko-add-welsh"),
           'can-mint': tx.falseCV(),
           'can-burn': tx.falseCV()
         })
