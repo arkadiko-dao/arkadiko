8bd935c30936e78570bf7861360040e363ad1d26
philipdesmedt <philip.desmedt@gmail.com>
2021-12-29 19:24:28 +0100

    tweak: make unlocking collateral clearer

--- web/components/manage-vault.tsx
+++ web/components/manage-vault.tsx
@@ -898,7 +898,7 @@ export const ManageVault = ({ match }) => {
                       <div className="mt-4">
                         <Alert>
                           <p>
-                            You have unstacked your collateral, you can choose to stack again.
+                            You have unstacked your collateral. It is still stacking in PoX. Once your cooldown cycle hits, you can unlock the collateral.
                           </p>
                         </Alert>
                       </div>
