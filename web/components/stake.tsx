72ffddee9be5f14f7b33c1b4a5c78dc5d6f9e7b3
philipdesmedt <philip.desmedt@gmail.com>
2022-01-04 12:39:07 +0100

    fix: show correct LP amount

--- web/components/stake.tsx
+++ web/components/stake.tsx
@@ -185,9 +185,9 @@ export const Stake = () => {
         tokenYName = 'xBTC';
         tokenYDecimals = 8;
       } else if (poolContract == 'arkadiko-stake-pool-xbtc-usda-v1-1') {
-        tokenXContract = 'usda-token';
         tokenXAddress = 'SP3DX3H4FEYZJZ586MFBS25ZW3HZDMEW92260R2PR';
         tokenXContract = 'Wrapped-Bitcoin';
+        tokenYContract = 'usda-token';
         tokenXName = 'xBTC';
         tokenYName = 'USDA';
       }
