736338a163b25db0a1762c4216e6eb6c324293fc
philipdesmedt <philip.desmedt@gmail.com>
2022-01-06 22:52:50 +0100

    return token on tokens#show

--- api/app/controllers/tokens_controller.rb
+++ api/app/controllers/tokens_controller.rb
@@ -8,6 +8,7 @@ class TokensController < ApplicationController
     pool = find_pool(token)
     event = pool.swap_events.order('event_at ASC').where("function_name IN (?)", ['swap-x-for-y', 'swap-y-for-x']).last
     render json: {
+      token: token,
       price_in_cents: 100 * (event['token_y_amount'] / event['token_x_amount'].to_f).round(4)
     }
   end
