b5a3d9c15477eed830b485517bfa8ca7dc3813df
philipdesmedt <philip.desmedt@gmail.com>
2022-01-06 22:52:03 +0100

    update token metrics method

--- api/app/controllers/tokens_controller.rb
+++ api/app/controllers/tokens_controller.rb
@@ -1,12 +1,39 @@
 class TokensController < ApplicationController
+  before_action :check_api_key, only: [:update]
+
   def show
-    if params[:id]&.downcase == 'diko'
-      event = Pool.find(2).swap_events.order('event_at ASC').where("function_name IN (?)", ['swap-x-for-y', 'swap-y-for-x']).last
-      render json: {
-        price_in_cents: 100 * (event['token_y_amount'] / event['token_x_amount'].to_f).round(4)
-      }
-    else
-      return head 404
-    end
+    token = Token.find_by('lower(symbol) = ?', params[:id].downcase)
+    return head 401 if token.nil?
+
+    pool = find_pool(token)
+    event = pool.swap_events.order('event_at ASC').where("function_name IN (?)", ['swap-x-for-y', 'swap-y-for-x']).last
+    render json: {
+      price_in_cents: 100 * (event['token_y_amount'] / event['token_x_amount'].to_f).round(4)
+    }
   end
+
+  def update
+    token = Token.find_by('lower(symbol) = ?', params[:id].downcase)
+    token.total_supply = params[:total_supply] if params[:total_supply].present?
+    token.last_price = params[:last_price] if params[:last_price].present?
+    token.price_last_updated = params[:price_last_updated] if params[:price_last_updated].present?
+    token.total_staked = params[:total_staked] if params[:total_staked].present?
+    token.save
+
+    render json: token
+  end
+
+  private
+
+    def find_pool(token)
+      if token.symbol == 'diko'
+        Pool.find(2)
+      elsif token.symbol == 'stx'
+        Pool.find(1)
+      end
+    end
+
+    def check_api_key
+      return head 403 unless params[:key] === ENV['API_KEY']
+    end
 end
