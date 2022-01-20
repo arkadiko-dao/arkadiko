3a0b534fe69977f4c3238e4f19ba18c9312238d3
philipdesmedt <philip.desmedt@gmail.com>
2022-01-20 11:16:51 +0100

    add oracle endpoint

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
     resources :pages, only: :none do
       collection do
         get 'stake'
+        get 'swap'
+        get 'oracle'
       end
     end
   end
