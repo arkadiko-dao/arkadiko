5b51577292c05a12d60ff6ee50065ce3b4c5f0fb
philipdesmedt <philip.desmedt@gmail.com>
2022-02-01 12:41:18 +0100

    upgrade to governance v3

--- scripts/governance-add-proposal.js
+++ scripts/governance-add-proposal.js
@@ -16,12 +16,12 @@ async function transact() {
       tx.uintCV(43100),
       tx.uintCV(900),
       tx.stringUtf8CV('Add STX/WELSH Pair'),
-      tx.stringUtf8CV('https://github.com/arkadiko-dao/arkadiko/pull/408'),
+      tx.stringUtf8CV('https://github.com/arkadiko-dao/arkadiko/pull/421'),
       tx.listCV([
         tx.tupleCV({
-          'name': tx.stringAsciiCV("add-welsh"),
+          'name': tx.stringAsciiCV("governance"),
           'address': tx.standardPrincipalCV("SP2C2YFP12AJZB4MABJBAJ55XECVS7E4PMMZ89YZR"),
-          'qualified-name': tx.contractPrincipalCV("SP2C2YFP12AJZB4MABJBAJ55XECVS7E4PMMZ89YZR", "arkadiko-add-welsh"),
+          'qualified-name': tx.contractPrincipalCV("SP2C2YFP12AJZB4MABJBAJ55XECVS7E4PMMZ89YZR", "arkadiko-governance-v3-1"),
           'can-mint': tx.falseCV(),
           'can-burn': tx.falseCV()
         })
