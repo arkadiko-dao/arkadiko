789359d87506cbb50e305a89e8e2b265f990b564
Niel <hello@nieldeckx.be>
2022-02-07 09:58:20 +0100

    Discounted auction price tests

--- clarity/tests/models/arkadiko-tests-vaults.ts
+++ clarity/tests/models/arkadiko-tests-vaults.ts
@@ -389,11 +389,11 @@ class VaultAuction {
     );
   }
 
-  getDiscountedAuctionPrice(price: number, auctionId: number) {
+  getDiscountedAuctionPrice(price: number, decimals: number, auctionId: number) {
     return this.chain.callReadOnlyFn(
       "arkadiko-auction-engine-v2-1",
       "discounted-auction-price",
-      [types.uint(price * 1000000), types.uint(auctionId)],
+      [types.uint(price * 1000000), types.uint(decimals), types.uint(auctionId)],
       this.deployer.address
     );
   }
