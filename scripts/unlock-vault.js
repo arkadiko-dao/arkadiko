4713aaff7bf184c6376d269e4e278278fd9fad19
philipdesmedt <philip.desmedt@gmail.com>
2022-02-23 09:20:39 +0100

    tweak: unlock vaults script through array

--- scripts/unlock-vault.js
+++ scripts/unlock-vault.js
@@ -3,8 +3,9 @@ const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
 const tx = require('@stacks/transactions');
 const utils = require('./utils');
 const network = utils.resolveNetwork();
+const BN = require('bn.js');
 
-async function unlockVault(vaultId) {
+async function unlockVault(vaultId, nonce) {
   const txOptions = {
     contractAddress: CONTRACT_ADDRESS,
     contractName: "arkadiko-freddie-v1-1",
@@ -12,12 +13,41 @@ async function unlockVault(vaultId) {
     functionArgs: [tx.uintCV(vaultId)],
     senderKey: process.env.STACKS_PRIVATE_KEY,
     postConditionMode: 1,
+    nonce: new BN(nonce, 10),
     network
   };
 
   const transaction = await tx.makeContractCall(txOptions);
   const result = tx.broadcastTransaction(transaction, network);
-  await utils.processing(result, transaction.txid(), 0);
+  return utils.processing(result, transaction.txid(), 0);
 }
 
-unlockVault(292);
+// 1628
+let nonce = 593;
+
+ids = [
+  1629,
+  1396,
+  1640,
+  1541,
+  1478,
+  1477,
+  1338,
+  1635,
+  1681,
+  1632,
+  1534,
+  1396
+];
+
+async function asyncForEach(array, callback) {
+  for (let index = 0; index < array.length; index++) {
+    await callback(array[index], index, array);
+  }
+}
+
+asyncForEach(ids, async (id) => {
+  console.log('unlock vault', id, 'with nonce', nonce);
+  await unlockVault(id, nonce);
+  nonce = nonce + 1;
+});
