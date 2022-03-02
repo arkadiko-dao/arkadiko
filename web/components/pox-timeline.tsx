104d6014d881f69f1aecae605e64e63c345ec2f6
Maxime Laforet <me@macx.im>
2022-03-02 21:12:53 +0000

    Extract PoxTimelineIndicator

--- web/components/pox-timeline.tsx
+++ web/components/pox-timeline.tsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { AppContext } from '@common/context';
 import { Tooltip } from '@blockstack/ui';
 import { Placeholder } from './ui/placeholder';
+import { PoxTimelineIndicator } from './pox-timeline-indicator';
 
 export const PoxTimeline = ({ unlockBurnHeight, currentBurnHeight, isLoading }) => {
   const [state, _] = useContext(AppContext);
@@ -37,69 +38,63 @@ export const PoxTimeline = ({ unlockBurnHeight, currentBurnHeight, isLoading }) 
                 Cycle 1 (#{firstCycle})
 
                 {currentIndex === 0 ? (
-                  <div className="absolute flex flex-col items-start justify-start transform -translate-x-1/2 cursor-help whitespace-nowrap left-3/4 -top-11">
-                    <div className="flex items-center order-1 mt-1 text-gray-600 dark:text-gray-300">
-                      <svg className="w-4 h-4 mr-1 rotate-180 -scale-x-100" viewBox="0 0 24 24" strokeWidth="3" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                        <path d="M18 18h-6a3 3 0 0 1 -3 -3v-10l-4 4m8 0l-4 -4"></path>
-                      </svg>
-                      <div className="text-xs font-semibold">We are here</div>
-                    </div>
-                    <a href={`https://mempool.space/`} target="_blank" rel="noopener noreferrer" className="font-normal hover:underline text-xs mt-0.5 ml-1 text-gray-500 dark:text-gray-200">#{currentBurnHeight}</a>
-                  </div>
+                  <PoxTimelineIndicator
+                    position="transform -translate-x-1/2 left-3/4 -top-11"
+                    reversed={true}
+                    arrow="rotate-180 -scale-x-100"
+                    label="We are here"
+                    link={`https://mempool.space/`}
+                    block={currentBurnHeight}
+                  />
                 ) : null}
               </div>
               <div className="relative flex items-center justify-center w-1/3 h-full text-xs font-semibold text-white bg-indigo-600/80">
                 Cycle 2 (#{firstCycle + 1})
 
                 {currentIndex === 1 ? (
-                  <div className="absolute flex flex-col items-start justify-start transform -translate-x-1/2 cursor-help whitespace-nowrap left-3/4 -top-11">
-                    <div className="flex items-center order-1 mt-1 text-gray-600 dark:text-gray-300">
-                      <svg className="w-4 h-4 mr-1 rotate-180 -scale-x-100" viewBox="0 0 24 24" strokeWidth="3" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                        <path d="M18 18h-6a3 3 0 0 1 -3 -3v-10l-4 4m8 0l-4 -4"></path>
-                      </svg>
-                      <div className="text-xs font-semibold">We are here</div>
-                    </div>
-                    <a href={`https://mempool.space/`} target="_blank" rel="noopener noreferrer" className="font-normal hover:underline text-xs mt-0.5 ml-1 text-gray-500 dark:text-gray-200">#{currentBurnHeight}</a>
-                  </div>
+                  <PoxTimelineIndicator
+                    position="transform -translate-x-1/2 left-3/4 -top-11"
+                    reversed={true}
+                    arrow="rotate-180 -scale-x-100"
+                    label="We are here"
+                    link={`https://mempool.space/`}
+                    block={currentBurnHeight}
+                  />
                 ) : null}
               </div>
               <div className="relative flex items-center justify-center w-1/3 h-full text-xs font-semibold text-white bg-indigo-600/90">
                 Cycle 3 (#{firstCycle + 2})
 
                 {currentIndex === 2 ? (
-                  <div className="absolute flex flex-col items-start justify-start transform -translate-x-1/2 cursor-help whitespace-nowrap left-3/4 -top-11">
-                    <div className="flex items-center order-1 mt-1 text-gray-600 dark:text-gray-300">
-                      <svg className="w-4 h-4 mr-1 rotate-180 -scale-x-100" viewBox="0 0 24 24" strokeWidth="3" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                        <path d="M18 18h-6a3 3 0 0 1 -3 -3v-10l-4 4m8 0l-4 -4"></path>
-                      </svg>
-                      <div className="text-xs font-semibold">We are here</div>
-                    </div>
-                    <a href={`https://mempool.space/`} target="_blank" rel="noopener noreferrer" className="font-normal hover:underline text-xs mt-0.5 ml-1 text-gray-500 dark:text-gray-200">#{currentBurnHeight}</a>
-                  </div>
+                  <PoxTimelineIndicator
+                    position="transform -translate-x-1/2 left-3/4 -top-11"
+                    reversed={true}
+                    arrow="rotate-180 -scale-x-100"
+                    label="We are here"
+                    link={`https://mempool.space/`}
+                    block={currentBurnHeight}
+                  />
                 ) : null}
 
-                <div className="absolute flex flex-col items-start justify-start cursor-help -right-2 top-11">
-                  <div className="flex items-center mt-1 text-gray-600 dark:text-gray-300">
-                    <svg className="order-1 w-4 h-4 mr-1 -scale-x-100" viewBox="0 0 24 24" strokeWidth="3" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                      <path d="M18 18h-6a3 3 0 0 1 -3 -3v-10l-4 4m8 0l-4 -4"></path>
-                    </svg>
-                    <div className="text-xs font-semibold">Finish</div>
-                  </div>
-                  <a href="https://mempool.space/" target="_blank" rel="noopener noreferrer" className="font-normal hover:underline text-xs mt-0.5 mr-1 text-gray-500 dark:text-gray-200">#{unlockBurnHeight}</a>
-                </div>
+                <PoxTimelineIndicator
+                  position="-right-2 top-11"
+                  arrow="order-1 w-4 h-4 mr-1 -scale-x-100"
+                  label="Finish"
+                  link={`https://mempool.space/`}
+                  block={unlockBurnHeight}
+                />
               </div>
             </div>
             <div className="flex text-white items-center font-semibold justify-center text-xs text-center w-[25%] bg-indigo-800 hover:cursor-help rounded-r-md">
               {currentIndex === 3 ? (
-                <div className="relative flex flex-col items-start justify-start transform -translate-x-1/2 cursor-help whitespace-nowrap left-3/4 -top-11">
-                  <div className="flex items-center order-1 mt-1 text-gray-600 dark:text-gray-300">
-                    <svg className="w-4 h-4 mr-1 rotate-180 -scale-x-100" viewBox="0 0 24 24" strokeWidth="3" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                      <path d="M18 18h-6a3 3 0 0 1 -3 -3v-10l-4 4m8 0l-4 -4"></path>
-                    </svg>
-                    <div className="text-xs font-semibold">We are here</div>
-                  </div>
-                  <a href={`https://mempool.space/`} target="_blank" rel="noopener noreferrer" className="font-normal hover:underline text-xs mt-0.5 ml-1 text-gray-500 dark:text-gray-200">#{currentBurnHeight}</a>
-                </div>
+                <PoxTimelineIndicator
+                  position="transform -translate-x-1/2 left-3/4 -top-11"
+                  reversed={true}
+                  arrow="rotate-180 -scale-x-100"
+                  label="We are here"
+                  link={`https://mempool.space/`}
+                  block={currentBurnHeight}
+                />
               ) : null}
               <Tooltip
                 label={`2-week period`}
@@ -109,15 +104,11 @@ export const PoxTimeline = ({ unlockBurnHeight, currentBurnHeight, isLoading }) 
             </div>
           </div>
 
-          <div className="absolute flex flex-col items-start justify-start cursor-help -left-1 top-11">
-            <div className="flex items-center mt-1 text-gray-600 dark:text-gray-300">
-              <svg className="w-4 h-4 mr-1" viewBox="0 0 24 24" strokeWidth="3" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                <path d="M18 18h-6a3 3 0 0 1 -3 -3v-10l-4 4m8 0l-4 -4"></path>
-              </svg>
-              <div className="text-xs font-semibold">Start</div>
-            </div>
-            <a href={`https://mempool.space/block/${state.cycleStartHeight}`} target="_blank" rel="noopener noreferrer" className="hover:underline text-xs mt-0.5 ml-1 text-gray-500 dark:text-gray-200">#{startBurnHeight}</a>
-          </div>
+          <PoxTimelineIndicator
+            label="Start"
+            link={`https://mempool.space/block/${state.cycleStartHeight}`}
+            block={startBurnHeight}
+          />
         </div>
       )}
     </>
