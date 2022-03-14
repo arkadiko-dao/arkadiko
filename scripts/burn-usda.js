1fb7a0dbf344aa6aa99e1addcfb9ea79345b2073
philipdesmedt <philip.desmedt@gmail.com>
2022-03-14 10:51:21 +0100

    tweak: add script to burn USDA

---
+++ scripts/burn-usda.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
+const tx = require('@stacks/transactions');
+const utils = require('./utils');
+const network = utils.resolveNetwork();
+const BN = require('bn.js');
+
+async function transact() {
+  const txOptions = {
+    contractAddress: CONTRACT_ADDRESS,
+    contractName: 'arkadiko-usda-burner',
+    functionName: 'burn-usda',
+    functionArgs: [tx.uintCV(150000000000)],
+    fee: new BN(10000, 10),
+    senderKey: process.env.STACKS_PRIVATE_KEY,
+    postConditionMode: 1,
+    network
+  };
+
+  const transaction = await tx.makeContractCall(txOptions);
+  const result = tx.broadcastTransaction(transaction, network);
+  await utils.processing(result, transaction.txid(), 0);
+};
+
+transact();
