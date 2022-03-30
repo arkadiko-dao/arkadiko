b60f840220ff09f7966f32bd1fe707d9ed41ec27
philipdesmedt <philip.desmedt@gmail.com>
2022-03-30 11:08:09 +0200

    misc: add script to redeem auction engine USDA

---
+++ scripts/auction-engine-redeem-usda.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
+const tx = require('@stacks/transactions');
+const utils = require('./utils');
+const network = utils.resolveNetwork();
+const BN = require('bn.js');
+
+const txOptions = {
+  contractAddress: CONTRACT_ADDRESS,
+  contractName: 'arkadiko-auction-engine-v3-1',
+  functionName: 'redeem-usda',
+  functionArgs: [tx.uintCV(5490069877)],
+  senderKey: process.env.STACKS_PRIVATE_KEY,
+  postConditionMode: 1,
+  network
+};
+
+async function transact() {
+  const transaction = await tx.makeContractCall(txOptions);
+  const result = tx.broadcastTransaction(transaction, network);
+  await utils.processing(result, transaction.txid(), 0);
+};
+
+transact();
