aed63552ee9c7cfa77997e6d61f26f6d56b88b62
philipdesmedt <philip.desmedt@gmail.com>
2022-07-13 20:11:00 +0200

    Roll back webpack

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -74,7 +74,50 @@ module.exports = {
     alias: aliases,
   },
   optimization: {
-    minimize: false,
+    minimize: !isDevelopment,
+    minimizer: [
+      // This is only used in production mode
+      new TerserPlugin({
+        terserOptions: {
+          parse: {
+            // We want terser to parse ecma 8 code. However, we don't want it
+            // to apply any minification steps that turns valid ecma 5 code
+            // into invalid ecma 5 code. This is why the 'compress' and 'output'
+            // sections only apply transformations that are ecma 5 safe
+            // https://github.com/facebook/create-react-app/pull/4234
+            ecma: 8,
+          },
+          compress: {
+            ecma: 5,
+            warnings: false,
+            // Disabled because of an issue with Uglify breaking seemingly valid code:
+            // https://github.com/facebook/create-react-app/issues/2376
+            // Pending further investigation:
+            // https://github.com/mishoo/UglifyJS2/issues/2011
+            comparisons: false,
+            // Disabled because of an issue with Terser breaking valid code:
+            // https://github.com/facebook/create-react-app/issues/5250
+            // Pending further investigation:
+            // https://github.com/terser-js/terser/issues/120
+            inline: 2,
+          },
+          mangle: {
+            safari10: true,
+          },
+          // Added for profiling in devtools
+          keep_classnames: !isDevelopment,
+          keep_fnames: !isDevelopment,
+          output: {
+            ecma: 5,
+            comments: false,
+            // Turned on because emoji and regex is not minified properly using default
+            // https://github.com/facebook/create-react-app/issues/2488
+            ascii_only: true,
+          },
+        },
+        sourceMap: false,
+      }),
+    ],
     // Automatically split vendor and commons
     // https://twitter.com/wSokra/status/969633336732905474
     // https://medium.com/webpack/webpack-4-code-splitting-chunk-graph-and-the-splitchunks-optimization-be739a861366
@@ -157,10 +200,7 @@ module.exports = {
       chunks: ['main', 'common'],
       ...hmtlProdOpts,
     }),
-    new webpack.ProvidePlugin({
-      // you must `npm install buffer` to use this.
-      Buffer: ['buffer', 'Buffer']
-    }),
+
     new CopyWebpackPlugin([
       {
         from: path.join(sourceRootPath, '../', 'public', 'assets'),
