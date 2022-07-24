a81e03d65b67662ea87b2c98cb71d3ba3cc26ad5
Niel <hello@nieldeckx.be>
2022-07-24 16:38:11 +0800

    update script to push Redstone oracle prices

--- scripts/update-price.js
+++ scripts/update-price.js
@@ -1,58 +1,15 @@
 require('dotenv').config();
 const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
-const CONTRACT_NAME = 'arkadiko-oracle-v1-1';
+const CONTRACT_NAME = 'arkadiko-oracle-v2-1';
 const FUNCTION_NAME = 'update-price';
-const rp = require('request-promise');
 const tx = require('@stacks/transactions');
+const redstone = require('redstone-api-extended');
 const BN = require('bn.js');
 const utils = require('./utils');
 const network = utils.resolveNetwork();
 
-const setPrice = async (stxPrice, btcPrice) => {
+async function updatePoolPrices() {
   let nonce = await utils.getNonce(CONTRACT_ADDRESS);
-  const priceWithDecimals = stxPrice.toFixed(4) * 1000000;
-
-  const txOptions = {
-    contractAddress: CONTRACT_ADDRESS,
-    contractName: CONTRACT_NAME,
-    functionName: FUNCTION_NAME,
-    functionArgs: [tx.stringAsciiCV('STX'), tx.uintCV(new BN(priceWithDecimals)), tx.uintCV(1000000)],
-    senderKey: process.env.STACKS_PRIVATE_KEY,
-    nonce: new BN(nonce),
-    postConditionMode: 1,
-    network
-  };
-  const transaction = await tx.makeContractCall(txOptions);
-  const result = tx.broadcastTransaction(transaction, network);
-  await utils.processing(result, transaction.txid(), 0);
-
-  const xTxOptions = {
-    contractAddress: CONTRACT_ADDRESS,
-    contractName: CONTRACT_NAME,
-    functionName: FUNCTION_NAME,
-    functionArgs: [tx.stringAsciiCV('xSTX'), tx.uintCV(new BN(priceWithDecimals)), tx.uintCV(1000000)],
-    senderKey: process.env.STACKS_PRIVATE_KEY,
-    nonce: new BN(nonce + 1),
-    postConditionMode: 1,
-    network
-  };
-  const transaction2 = await tx.makeContractCall(xTxOptions);
-  const result2 = tx.broadcastTransaction(transaction2, network);
-  await utils.processing(result2, transaction2.txid(), 0);
-
-  const xBtcTxOptions = {
-    contractAddress: CONTRACT_ADDRESS,
-    contractName: CONTRACT_NAME,
-    functionName: FUNCTION_NAME,
-    functionArgs: [tx.stringAsciiCV('xBTC'), tx.uintCV(new BN(btcPrice.toFixed(0) * 1000000)), tx.uintCV(100000000)],
-    senderKey: process.env.STACKS_PRIVATE_KEY,
-    nonce: new BN(nonce + 2),
-    postConditionMode: 1,
-    network
-  };
-  const transaction3 = await tx.makeContractCall(xBtcTxOptions);
-  const result3 = tx.broadcastTransaction(transaction3, network);
-  await utils.processing(result3, transaction3.txid(), 0);
 
   const fetchPair = async () => {
     let details = await tx.callReadOnlyFunction({
@@ -66,7 +23,6 @@ const setPrice = async (stxPrice, btcPrice) => {
       senderAddress: CONTRACT_ADDRESS,
       network: network,
     });
-
     return tx.cvToJSON(details);
   };
 
@@ -85,30 +41,88 @@ const setPrice = async (stxPrice, btcPrice) => {
       postConditionMode: 1,
       network
     };
-    const transaction4 = await tx.makeContractCall(dikoTxOptions);
-    const result4 = tx.broadcastTransaction(transaction4, network);
-    await utils.processing(result4, transaction4.txid(), 0);
+    const transaction = await tx.makeContractCall(dikoTxOptions);
+    const result = tx.broadcastTransaction(transaction, network);
+    await utils.processing(result, transaction.txid(), 0);
   }
 };
 
-const requestOptions = {
-  method: 'GET',
-  uri: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest',
-  qs: {
-    'id': '4847,1',
-    'convert': 'USD'
-  },
-  headers: {
-    'X-CMC_PRO_API_KEY': process.env.CMC_API_KEY
-  },
-  json: true,
-  gzip: true
-};
+function hexToBytes(hex) {
+  if (typeof hex !== 'string')
+    throw new TypeError('hexToBytes: expected string, got ' + typeof hex);
+  if (hex.length % 2)
+    throw new Error(`hexToBytes: received invalid unpadded hex, got: ${hex.length}`);
+  const array = new Uint8Array(hex.length / 2);
+  for (let i = 0; i < array.length; i++) {
+    const j = i * 2;
+    array[i] = Number.parseInt(hex.slice(j, j + 2), 16);
+  }
+  return array;
+}
 
-rp(requestOptions).then(async (response) => {
-  let stxPrice = response['data']['4847']['quote']['USD']['price'];
-  let btcPrice = response['data']['1']['quote']['USD']['price'];
-  await setPrice(stxPrice, btcPrice);
-});
+function liteSignatureToStacksSignature(liteSignature) {
+	if (typeof liteSignature === 'string')
+		liteSignature = hexToBytes(liteSignature.substring(0, 2) === '0x' ? liteSignature.substring(2) : liteSignature);
+	if (liteSignature.byteLength !== 65)
+		throw new Error(`Invalid liteSignature, expected 65 bytes got ${liteSignature.byteLength}`);
+	let converted = new Uint8Array(liteSignature);
+	if (converted[64] > 3)
+		converted[64] -= 27;
+	return converted;
+}
 
-// setPrice(1.15, 45000); // 0.5 USD
+async function updateRedstonePrices(timestamp, stxPrice, btcPrice, stxSig, btcSig) {
+  let nonce = await utils.getNonce(CONTRACT_ADDRESS);
+
+  const txOptions = {
+    contractAddress: CONTRACT_ADDRESS,
+    contractName: CONTRACT_NAME,
+    functionName: "update-prices-redstone",
+    functionArgs: [
+      tx.uintCV(timestamp),
+      tx.listCV([
+        tx.tupleCV({
+          symbol: tx.bufferCVFromString("STX"),
+          value: tx.uintCV(stxPrice),
+        }),
+        tx.tupleCV({
+          symbol: tx.bufferCVFromString("BTC"),
+          value: tx.uintCV(btcPrice),
+        })
+      ]),
+      tx.listCV([
+        tx.bufferCV(stxSig), 
+        tx.bufferCV(btcSig)
+      ])
+    ],
+    senderKey: process.env.STACKS_PRIVATE_KEY,
+    nonce: new BN(nonce),
+    postConditionMode: 1,
+    network
+  };
+  const transaction = await tx.makeContractCall(txOptions);
+  const result = tx.broadcastTransaction(transaction, network);
+  await utils.processing(result, transaction.txid(), 0);
+}
+
+async function start() {
+
+  console.log("Update oracle prices..");
+
+  const dataStx = await redstone.oracle.getFromDataFeed("redstone", "STX");
+  const dataBtc = await redstone.oracle.getFromDataFeed("redstone", "BTC");
+
+  console.log(dataStx);
+  console.log(dataBtc);
+
+  const timestamp = dataStx.priceData.timestamp
+  const stxPrice = dataStx.priceData.values[0];
+  const btcPrice = dataBtc.priceData.values[0];
+  const stxSig = Buffer.from(liteSignatureToStacksSignature(dataStx.liteSignature));
+  const btcSig = Buffer.from(liteSignatureToStacksSignature(dataBtc.liteSignature));
+
+  await updateRedstonePrices(timestamp, stxPrice, btcPrice, stxSig, btcSig);
+  await updatePoolPrices();
+}
+
+start()
