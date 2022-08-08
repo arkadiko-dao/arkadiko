5a23526e8296554ea55ae175d54970c809978000
philipdesmedt <philip.desmedt@gmail.com>
2022-08-08 11:47:16 +0200

    show atALEX price in front-end

--- scripts/collateral-type-change-token-address.js
+++ scripts/collateral-type-change-token-address.js
@@ -11,12 +11,11 @@ async function transact() {
     contractName: 'arkadiko-collateral-types-v1-1',
     functionName: 'change-token-address',
     functionArgs: [
-      tx.stringAsciiCV('STX-A'),
+      tx.stringAsciiCV('ATALEX-A'),
       tx.contractPrincipalCV('SP3K8BC0PPEVCV7NZ6QSRWPQ2JE9E5B6N3PA0KBR9', 'auto-alex')
     ],
     senderKey: process.env.STACKS_PRIVATE_KEY,
     postConditionMode: 1,
-    nonce: new BN(2071, 10),
     network
   };
 
