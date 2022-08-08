362cffc8d9e7804ac9ea029902e1201d1a502fd4
philipdesmedt <philip.desmedt@gmail.com>
2022-08-08 10:14:47 +0200

    TX to add atALEX collateral type

--- scripts/collateral-type-change-risk-parameter.js
+++ scripts/collateral-type-change-risk-parameter.js
@@ -7,18 +7,25 @@ const BN = require('bn.js');
 
 async function transact() {
   const list = tx.listCV([
-    tx.tupleCV({ 'key': tx.stringAsciiCV('maximum-debt'), 'new-value': tx.uintCV(100000000) })
+    tx.tupleCV({ 'key': tx.stringAsciiCV('name'), 'new-value': tx.stringAsciiCV('Auto ALEX') }),
+    tx.tupleCV({ 'key': tx.stringAsciiCV('token'), 'new-value': tx.stringAsciiCV('atALEX') }),
+    tx.tupleCV({ 'key': tx.stringAsciiCV('token-type'), 'new-value': tx.stringAsciiCV('ATALEX-A') }),
+    tx.tupleCV({ 'key': tx.stringAsciiCV('token-address'), 'new-value': tx.contractPrincipalCV('SP3K8BC0PPEVCV7NZ6QSRWPQ2JE9E5B6N3PA0KBR9', 'auto-alex') }),
+    tx.tupleCV({ 'key': tx.stringAsciiCV('url'), 'new-value': tx.stringAsciiCV('https://alexlab.co/') }),
+    tx.tupleCV({ 'key': tx.stringAsciiCV('liquidation-ratio'), 'new-value': tx.uintCV(new BN(180)) }),
+    tx.tupleCV({ 'key': tx.stringAsciiCV('collateral-to-debt-ratio'), 'new-value': tx.uintCV(new BN(400)) }),
+    tx.tupleCV({ 'key': tx.stringAsciiCV('maximum-debt'), 'new-value': tx.uintCV(new BN(0)) }),
+    tx.tupleCV({ 'key': tx.stringAsciiCV('liquidation-penalty'), 'new-value': tx.uintCV(new BN(2000)) })
   ]);
   const txOptions = {
     contractAddress: CONTRACT_ADDRESS,
     contractName: 'arkadiko-collateral-types-v1-1',
     functionName: 'change-risk-parameters',
     functionArgs: [
-      tx.stringAsciiCV('XBTC-A'),
+      tx.stringAsciiCV('ATALEX-A'),
       list
     ],
     senderKey: process.env.STACKS_PRIVATE_KEY,
-    fee: new BN(3000000, 10),
     postConditionMode: 1,
     network
   };
