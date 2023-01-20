3dc90a70ab01f65ca6f15deb20588b360953d980
Niel Deckx <hello@nieldeckx.be>
2023-01-20 14:47:39 +0800

    updated scripts

--- scripts/new-emissions-4.js
+++ scripts/new-emissions-4.js
@@ -12,9 +12,9 @@ async function transact() {
     contractName: 'arkadiko-governance-v4-1',
     functionName: 'add-contract-address',
     functionArgs: [
-      tx.stringAsciiCV('stake-pool-xusd-usda'),
+      tx.stringAsciiCV('stake-pool-xusd-usda-3'),
       tx.standardPrincipalCV(CONTRACT_ADDRESS),
-      tx.contractPrincipalCV(CONTRACT_ADDRESS, 'arkadiko-stake-pool-xusd-usda-v1-1'),
+      tx.contractPrincipalCV(CONTRACT_ADDRESS, 'arkadiko-stake-pool-xusd-usda-v1-3'),
       tx.trueCV(),
       tx.trueCV(),
     ],
