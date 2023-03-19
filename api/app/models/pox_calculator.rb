09334b736b78bd6cc9ed6d759cf83c51ce2459bd
philipdesmedt <philip.desmedt@gmail.com>
2023-03-19 20:42:12 +0100

    misc: add pox yields for cycle 54

--- api/app/models/pox_calculator.rb
+++ api/app/models/pox_calculator.rb
@@ -98,7 +98,7 @@ class PoxCalculator
     end
   end
 
-  def self.build_clarity_list_tuples(names:, file: 'vaults-pox-52.json', yield_type: 'ustx', ustx_stacked: 4_503_969_100_596, yield_earned: 10_200_000_000)
+  def self.build_clarity_list_tuples(names:, file: 'vaults-pox-53.json', yield_type: 'ustx', ustx_stacked: 3_847_895_079421, yield_earned: 6_085_000_000)
     if yield_type == 'usda'
       vaults = calculate_usda_yields(names: names, file: file, ustx_stacked: ustx_stacked, usda_yield: yield_earned)
     else
