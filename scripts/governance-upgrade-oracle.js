5272b00e6f85cb3522516d7f02bd62a6564ed7f5
--global <--global>
2023-07-10 20:43:43 +0200

    add governance tx

---
+++ scripts/governance-upgrade-oracle.js
@@ -0,0 +1,40 @@
+// node proposal-change-stacker.js
+require('dotenv').config();
+const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
+const tx = require('@stacks/transactions');
+const utils = require('./utils');
+const network = utils.resolveNetwork();
+const BN = require('bn.js');
+
+async function transact() {
+  const txOptions = {
+    contractAddress: CONTRACT_ADDRESS,
+    contractName: 'arkadiko-governance-v4-1',
+    functionName: 'propose',
+    functionArgs: [
+      tx.contractPrincipalCV(CONTRACT_ADDRESS, 'arkadiko-stake-pool-diko-v1-2'),
+      tx.uintCV(112150),
+      tx.uintCV(1440),
+      tx.stringUtf8CV('Upgrade Arkadiko Oracle to v2.2 (Decentralised)'),
+      tx.stringUtf8CV('https://github.com/arkadiko-dao/arkadiko/pull/524'),
+      tx.listCV([
+        tx.tupleCV({
+          'name': tx.stringAsciiCV("oracle"),
+          'address': tx.standardPrincipalCV("SP2C2YFP12AJZB4MABJBAJ55XECVS7E4PMMZ89YZR"),
+          'qualified-name': tx.contractPrincipalCV("SP2C2YFP12AJZB4MABJBAJ55XECVS7E4PMMZ89YZR", "arkadiko-oracle-v2-2"),
+          'can-mint': tx.falseCV(),
+          'can-burn': tx.falseCV()
+        }),
+      ])
+    ],
+    senderKey: process.env.STACKS_PRIVATE_KEY,
+    postConditionMode: 1,
+    network
+  };
+
+  const transaction = await tx.makeContractCall(txOptions);
+  const result = tx.broadcastTransaction(transaction, network);
+  await utils.processing(result, transaction.txid(), 0);
+};
+
+transact();
