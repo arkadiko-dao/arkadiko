71b0a34e00611e57d370b761c9e9f70ad503190f
--global <--global>
2023-10-13 00:23:13 +0200

    finish step 1

--- web/components/create-vault-step-one.tsx
+++ web/components/create-vault-step-one.tsx
@@ -3,7 +3,7 @@ import { AppContext } from '@common/context';
 import { CollateralType } from '@components/collateral-type';
 import { NewVaultWizardNav } from './new-vault-wizard-nav';
 import { tokenList } from '@components/token-swap-list';
-import { useHistory } from 'react-router-dom';
+import { useHistory, NavLink as RouterLink } from 'react-router-dom';
 
 interface VaultProps {
   setStep: (arg: number) => void;
@@ -19,16 +19,6 @@ export const CreateVaultStepOne: React.FC<VaultProps> = ({ setStep }) => {
     setStep(0);
   }, []);
 
-  const stxCollateralTypes = Object.fromEntries(
-    Object.entries(collateralTypes).filter(([key]) => key.includes('STX'))
-  );
-  const xBtcCollateralTypes = Object.fromEntries(
-    Object.entries(collateralTypes).filter(([key]) => key.includes('XBTC'))
-  );
-  const atAlexCollateralTypes = Object.fromEntries(
-    Object.entries(collateralTypes).filter(([key]) => key.includes('ATALEX'))
-  );
-
   return (
     <>
       <NewVaultWizardNav currentSection={currentSection} setStep={setStep} />
@@ -42,69 +32,35 @@ export const CreateVaultStepOne: React.FC<VaultProps> = ({ setStep }) => {
 
         <div className="flex flex-col mt-4">
           <div className="flex items-center justify-center mb-6">
-            <button
-              type="button"
-              className={`w-1/2 p-6 text-lg font-semibold text-center text-gray-500 dark:text-zinc-300 bg-white dark:bg-zinc-700 rounded-md shadow md:w-1/6 hover:bg-white/80 ${
-                collateralTypeChoice === 'stx' || (location.hash.indexOf('stx') === 1)
-                  ? 'border border-indigo-500/60'
-                  : 'border border-transparent'
-              }`}
-              onClick={
-                () => {
-                  setCollateralTypeChoice('stx');
-                  history.push(`#stx`);
-                }
-              }
+            <RouterLink
+              to={'/vaults/new?token=stx'}
+              onClick={() => setStep(1)}
+              exact
+              className="border border-transparent w-1/2 p-6 text-lg font-semibold text-center text-gray-500 dark:text-zinc-300 bg-white dark:bg-zinc-700 rounded-md shadow md:w-1/6 hover:bg-white/80"
             >
               <img className="w-10 h-10 mx-auto mb-3 rounded-full" src={tokenList[2].logo} alt="" />
               STX
-            </button>
+            </RouterLink>
 
-            <button
-              type="button"
-              className={`w-1/2 p-6 ml-6 text-lg font-semibold text-center text-gray-500 dark:text-zinc-300 bg-white dark:bg-zinc-700 rounded-md shadow md:w-1/6 hover:bg-white/80 ${
-                collateralTypeChoice === 'xbtc' || (location.hash.indexOf('xbtc') === 1)
-                  ? 'border border-indigo-500/60'
-                  : 'border border-transparent'
-              }`}
-              onClick={
-                () => {
-                  setCollateralTypeChoice('xbtc');
-                  history.push(`#xbtc`);
-                }
-              }
+            <RouterLink
+              to={'/vaults/new?token=xbtc'}
+              onClick={() => setStep(1)}
+              exact
+              className="border border-transparent w-1/2 p-6 ml-6 text-lg font-semibold text-center text-gray-500 dark:text-zinc-300 bg-white dark:bg-zinc-700 rounded-md shadow md:w-1/6 hover:bg-white/80"
             >
               <img className="w-10 h-10 mx-auto mb-3 rounded-full" src={tokenList[3].logo} alt="" />
               xBTC
-            </button>
+            </RouterLink>
 
-            <button
-              type="button"
-              className={`w-1/2 p-6 ml-6 text-lg font-semibold text-center text-gray-500 dark:text-zinc-300 bg-white dark:bg-zinc-700 rounded-md shadow md:w-1/6 hover:bg-white/80 ${
-                collateralTypeChoice === 'auto-alex' || (location.hash.indexOf('auto-alex') === 1)
-                  ? 'border border-indigo-500/60'
-                  : 'border border-transparent'
-              }`}
-              onClick={
-                () => {
-                  setCollateralTypeChoice('auto-alex');
-                  history.push(`#auto-alex`);
-                }
-              }
+            <RouterLink
+              to={'/vaults/new?token=atalex'}
+              onClick={() => setStep(1)}
+              exact
+              className="border border-transparent w-1/2 p-6 ml-6 text-lg font-semibold text-center text-gray-500 dark:text-zinc-300 bg-white dark:bg-zinc-700 rounded-md shadow md:w-1/6 hover:bg-white/80"
             >
               <img className="w-10 h-10 mx-auto mb-3 rounded-full" src={tokenList[7].logo} alt="" />
               atALEX
-            </button>
-          </div>
-
-          <div className="md:max-w-4xl md:mx-auto">
-            {collateralTypeChoice === 'stx' || (location.hash.indexOf('stx') === 1) ? (
-              <CollateralType types={stxCollateralTypes} setStep={setStep} />
-            ) : collateralTypeChoice === 'xbtc' || (location.hash.indexOf('xbtc') === 1) ? (
-              <CollateralType types={xBtcCollateralTypes} setStep={setStep} />
-            ) : collateralTypeChoice === 'auto-alex' || (location.hash.indexOf('auto-alex') === 1) ? (
-              <CollateralType types={atAlexCollateralTypes} setStep={setStep} />
-            ) : null}
+            </RouterLink>
           </div>
         </div>
       </section>
