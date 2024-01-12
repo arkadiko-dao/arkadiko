c6e556f5478f5b17235fa67a3e7123c96a03dca2
--global <--global>
2024-01-12 09:28:17 +0100

    chore: add script to increase stacking

---
+++ scripts/increase-stacking.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
+const tx = require('@stacks/transactions');
+const utils = require('./utils');
+const network = utils.resolveNetwork();
+const BN = require('bn.js');
+const stacking = require('@stacks/stacking');
+const c32 = require('c32check');
+
+async function extendStacking() {
+  const btcAddr = c32.c32ToB58('SP2C2YFP12AJZB4MABJBAJ55XECVS7E4PMMZ89YZR');
+  console.log(btcAddr);
+  const { hashMode, data } = stacking.decodeBtcAddress(btcAddr);
+  const hashbytes = tx.bufferCV(data);
+  const txOptions = {
+    contractAddress: CONTRACT_ADDRESS,
+    contractName: "arkadiko-stacker-4-v3-1",
+    functionName: "stack-increase",
+    functionArgs: [
+      tx.stringAsciiCV('stacker-4'),
+      tx.uintCV(200000000)
+    ],
+    fee: new BN(500000, 10),
+    senderKey: process.env.STACKS_PRIVATE_KEY,
+    postConditionMode: 1,
+    network
+  };
+
+  const transaction = await tx.makeContractCall(txOptions);
+  const result = tx.broadcastTransaction(transaction, network);
+  await utils.processing(result, transaction.txid(), 0);
+}
+
+extendStacking();
