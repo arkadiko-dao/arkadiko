dcb338f1917debedaece57f821f20844d0a77548
Maxime Laforet <me@macx.im>
2024-03-17 12:41:23 +0000

    add migration notice (usda stake pool)

--- web/components/collateral-card.tsx
+++ web/components/collateral-card.tsx
@@ -587,7 +587,7 @@ export const CollateralCard: React.FC<CollateralTypeProps> = () => {
                   <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
                   <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
                 </svg>
-                <p className="text-sm"><a className="font-medium text-indigo-500 dark:text-indigo-300 dark:hover:text-indigo-200 hover:text-indigo-700" href="#">Learn more</a></p>
+                <p className="text-sm"><a className="font-medium text-indigo-500 hover:text-indigo-700" href="#">Learn more</a></p>
               </div>
             </div>
           </div>
