f0d160c52ccb0bd1610a6d6f882835f289675107
--global <--global>
2024-03-19 08:02:31 +0100

    fix: add url for learn more

--- web/components/liquidations.tsx
+++ web/components/liquidations.tsx
@@ -662,7 +662,7 @@ export const Liquidations: React.FC = () => {
                           <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
                           <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
                         </svg>
-                        <p className="text-sm"><a className="font-medium text-indigo-500 hover:text-indigo-700" href="#">Learn more</a></p>
+                        <p className="text-sm"><a className="font-medium text-indigo-500 hover:text-indigo-700" target="_blank" href="https://twitter.com/ArkadikoFinance/status/1769034597394710594">Learn more</a></p>
                       </div>
                     </div>
                   </div>
