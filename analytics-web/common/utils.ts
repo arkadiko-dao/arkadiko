77ca49bac5c0bec392e090c125a16befbd5df149
--global <--global>
2024-03-20 19:53:06 +0100

    fix: use Hiro API

--- analytics-web/common/utils.ts
+++ analytics-web/common/utils.ts
@@ -1,7 +1,7 @@
 import { RPCClient } from '@stacks/rpc-client';
 import { StacksMainnet } from '@stacks/network';
 
-let coreApiUrl = 'https://small-solemn-frost.stacks-mainnet.quiknode.pro/deaf86bafdfbef850e40cdf5fa22c41cd447cdff';
+let coreApiUrl = 'https://api.hiro.so';
 
 export const getRPCClient = () => {
   return new RPCClient(coreApiUrl);
