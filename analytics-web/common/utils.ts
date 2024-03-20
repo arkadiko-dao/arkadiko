32675462e6557f0ec139259b6f134de1edff1996
--global <--global>
2024-03-20 13:56:53 +0100

    fix: use quicknode api for analytics

--- analytics-web/common/utils.ts
+++ analytics-web/common/utils.ts
@@ -1,7 +1,7 @@
 import { RPCClient } from '@stacks/rpc-client';
 import { StacksMainnet } from '@stacks/network';
 
-let coreApiUrl = 'https://api.hiro.so'; // TODO: fetch from .env
+let coreApiUrl = 'https://small-solemn-frost.stacks-mainnet.quiknode.pro/deaf86bafdfbef850e40cdf5fa22c41cd447cdff';
 
 export const getRPCClient = () => {
   return new RPCClient(coreApiUrl);
