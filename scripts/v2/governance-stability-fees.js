f4aa825002c859cd261a4e5b44fdea8f14857f2e
--global <--global>
2024-05-16 09:56:34 +0200

    wip

--- scripts/v2/governance-stability-fees.js
+++ scripts/v2/governance-stability-fees.js
@@ -15,10 +15,10 @@ async function transact() {
       tx.uintCV(843680),
       tx.uintCV(720),
       tx.stringUtf8CV('AIP 21b - Stability Fee Decrease'),
-      tx.stringUtf8CV('https://github.com/arkadiko-dao/arkadiko/pull/585'),
+      tx.stringUtf8CV('https://github.com/arkadiko-dao/arkadiko/pull/590'),
       tx.listCV([
         tx.tupleCV({
-          'name': tx.stringAsciiCV("aip-21-stability-fees"),
+          'name': tx.stringAsciiCV("aip-21-b-stability-fees"),
           'address': tx.standardPrincipalCV("SP2C2YFP12AJZB4MABJBAJ55XECVS7E4PMMZ89YZR"),
           'qualified-name': tx.contractPrincipalCV("SP2C2YFP12AJZB4MABJBAJ55XECVS7E4PMMZ89YZR", "aip-21-stability-fees"),
           'can-mint': tx.falseCV(),
