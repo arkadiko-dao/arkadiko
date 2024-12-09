e267af76d5dabd5e006b5ace39d8d8c8fb9b9677
--global <--global>
2024-12-09 10:20:54 +0100

    add burn DIKO script

---
+++ scripts/v2/burn-diko.js
@@ -0,0 +1,25 @@
+require('dotenv').config({path: '../.env'});
+const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
+const tx = require('@stacks/transactions');
+const utils = require('../utils');
+const network = utils.resolveNetwork();
+const BN = require('bn.js');
+
+async function transact() {
+  const txOptions = {
+    contractAddress: CONTRACT_ADDRESS,
+    contractName: 'arkadiko-diko-incinerator-v1',
+    functionName: 'burn',
+    functionArgs: [tx.uintCV(46469 * 1000000)],
+    fee: new BN(10000, 10),
+    senderKey: process.env.STACKS_PRIVATE_KEY,
+    postConditionMode: 1,
+    network
+  };
+
+  const transaction = await tx.makeContractCall(txOptions);
+  const result = tx.broadcastTransaction(transaction, network);
+  await utils.processing(result, transaction.txid(), 0);
+};
+
+transact();
