fa1e52eecadfb511fc4fe0ac751b6392c26826d0
Philip <philip@Philips-MacBook-Pro.local>
2025-01-17 12:54:40 +0100

    wip

--- scripts/v2/governance-stability-fees.js
+++ scripts/v2/governance-stability-fees.js
@@ -12,10 +12,10 @@ async function transact() {
     functionName: 'propose',
     functionArgs: [
       tx.contractPrincipalCV(CONTRACT_ADDRESS, 'arkadiko-stake-pool-diko-v2-1'),
-      tx.uintCV(872050),
+      tx.uintCV(879632),
       tx.uintCV(720),
       tx.stringUtf8CV('AIP 28 - Stability Fees'),
-      tx.stringUtf8CV('https://github.com/arkadiko-dao/arkadiko/pull/600'),
+      tx.stringUtf8CV('https://github.com/arkadiko-dao/arkadiko/pull/606'),
       tx.listCV([
         tx.tupleCV({
           'name': tx.stringAsciiCV("aip-28-stability-fees"),
@@ -26,15 +26,14 @@ async function transact() {
         }),
       ])
     ],
-    fee: new BN(100000, 10),
+    fee: 100000,
     senderKey: process.env.STACKS_PRIVATE_KEY,
-    postConditionMode: 1,
-    network
+    postConditionMode: 1
   };
 
   const transaction = await tx.makeContractCall(txOptions);
-  const result = tx.broadcastTransaction(transaction, network);
-  await utils.processing(result, transaction.txid(), 0);
+  const result = tx.broadcastTransaction({ transaction: transaction });
+  console.log(result);
 };
 
 transact();
