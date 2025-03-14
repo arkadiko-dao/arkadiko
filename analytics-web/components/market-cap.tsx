790c50a59dd7d2912b99bee525becf5426e8486e
Philip <philip@Philips-MacBook-Pro.local>
2025-03-14 14:12:20 +0100

    chore: burn DIKO

--- analytics-web/components/market-cap.tsx
+++ analytics-web/components/market-cap.tsx
@@ -33,7 +33,7 @@ export const MarketCap: React.FC = ({ lastDikoPrice, lastUsdaPrice }) => {
       const teamTokens = 21000000 - (30 * 475000);
       const lockedTokens = foundationTokens + teamTokens;
       const emissionTokens = totalSupply - lockedTokens;
-      const burnedTokens = 58000 + 46469 + 46148 + 66632 + 75744 + 83715 + 85309 + 120265 + 131964 + 127169 + 197085 + 164744 + 195000; // TODO: update every burn TX
+      const burnedTokens = 58000 + 46469 + 46148 + 66632 + 75744 + 83715 + 85309 + 120265 + 131964 + 127169 + 197085 + 164744 + 195000 + 244000; // TODO: update every burn TX
 
       setIsLoading(false);
       setDikoTotalSupply(100000000 - burnedTokens);
