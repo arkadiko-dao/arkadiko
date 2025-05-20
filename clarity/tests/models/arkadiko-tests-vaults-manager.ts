3c992f42191b4fba4f068a0984c23e672e82f86c
Niel Deckx <hello@nieldeckx.be>
2025-05-20 12:07:24 +0200

    fix: redemption fee underflow (#592)
    * fix: redemption fee underflow
    
    * fix: tests
    
    * fix: tests
    
    * fix: arkadiko-vaults-manager-v1-2
    
    * fix: governance proposal script
    
    * redemptions v1-2
    
    ---------
    
    Co-authored-by: --global <--global>
    Co-authored-by: Philip <philip@Philips-MacBook-Pro.local>

--- clarity/tests/models/arkadiko-tests-vaults-manager.ts
+++ clarity/tests/models/arkadiko-tests-vaults-manager.ts
@@ -6,7 +6,7 @@ import {
   types,
 } from "https://deno.land/x/clarinet/index.ts";
 
-import * as Utils from './arkadiko-tests-utils.ts';
+import * as Utils from "./arkadiko-tests-utils.ts";
 
 // ---------------------------------------------------------
 // Vaults Manager
@@ -22,49 +22,61 @@ class VaultsManager {
   }
 
   getShutdownActivated() {
-    return this.chain.callReadOnlyFn("arkadiko-vaults-manager-v1-1", "get-shutdown-activated", [
-    ], this.deployer.address);
+    return this.chain.callReadOnlyFn(
+      "arkadiko-vaults-manager-v1-2",
+      "get-shutdown-activated",
+      [],
+      this.deployer.address
+    );
   }
 
   getRedemptionBlockLast(token: string) {
-    return this.chain.callReadOnlyFn("arkadiko-vaults-manager-v1-1", "get-redemption-block-last", [
-      types.principal(Utils.qualifiedName(token)),
-    ], this.deployer.address);
+    return this.chain.callReadOnlyFn(
+      "arkadiko-vaults-manager-v1-2",
+      "get-redemption-block-last",
+      [types.principal(Utils.qualifiedName(token))],
+      this.deployer.address
+    );
   }
 
-  liquidateVault(
-    owner: string,
-    token: string,
-  ) {
+  liquidateVault(owner: string, token: string) {
     let block = this.chain.mineBlock([
-      Tx.contractCall("arkadiko-vaults-manager-v1-1", "liquidate-vault", [
-        types.principal(Utils.qualifiedName('arkadiko-vaults-tokens-v1-1')),
-        types.principal(Utils.qualifiedName('arkadiko-vaults-data-v1-1')),
-        types.principal(Utils.qualifiedName('arkadiko-vaults-sorted-v1-1')),
-        types.principal(Utils.qualifiedName('arkadiko-vaults-pool-active-v1-1')),
-        types.principal(Utils.qualifiedName('arkadiko-vaults-pool-liq-v1-2')),
-        types.principal(Utils.qualifiedName('arkadiko-vaults-helpers-v1-1')),
-        types.principal(Utils.qualifiedName('arkadiko-oracle-v2-3')),
-        types.principal(owner),
-        types.principal(Utils.qualifiedName(token)),
-      ], this.deployer.address)
+      Tx.contractCall(
+        "arkadiko-vaults-manager-v1-2",
+        "liquidate-vault",
+        [
+          types.principal(Utils.qualifiedName("arkadiko-vaults-tokens-v1-1")),
+          types.principal(Utils.qualifiedName("arkadiko-vaults-data-v1-1")),
+          types.principal(Utils.qualifiedName("arkadiko-vaults-sorted-v1-1")),
+          types.principal(
+            Utils.qualifiedName("arkadiko-vaults-pool-active-v1-1")
+          ),
+          types.principal(Utils.qualifiedName("arkadiko-vaults-pool-liq-v1-2")),
+          types.principal(Utils.qualifiedName("arkadiko-vaults-helpers-v1-1")),
+          types.principal(Utils.qualifiedName("arkadiko-oracle-v2-3")),
+          types.principal(owner),
+          types.principal(Utils.qualifiedName(token)),
+        ],
+        this.deployer.address
+      ),
     ]);
     return block.receipts[0].result;
   }
 
-  getCollateralForLiquidation(
-    token: string,
-    collateral: number,
-    debt: number
-  ) {
+  getCollateralForLiquidation(token: string, collateral: number, debt: number) {
     let block = this.chain.mineBlock([
-      Tx.contractCall("arkadiko-vaults-manager-v1-1", "get-collateral-for-liquidation", [
-        types.principal(Utils.qualifiedName('arkadiko-vaults-tokens-v1-1')),
-        types.principal(Utils.qualifiedName('arkadiko-oracle-v2-3')),
-        types.principal(Utils.qualifiedName(token)),
-        types.uint(collateral * 1000000),
-        types.uint(debt * 1000000),
-      ], this.deployer.address)
+      Tx.contractCall(
+        "arkadiko-vaults-manager-v1-2",
+        "get-collateral-for-liquidation",
+        [
+          types.principal(Utils.qualifiedName("arkadiko-vaults-tokens-v1-1")),
+          types.principal(Utils.qualifiedName("arkadiko-oracle-v2-3")),
+          types.principal(Utils.qualifiedName(token)),
+          types.uint(collateral * 1000000),
+          types.uint(debt * 1000000),
+        ],
+        this.deployer.address
+      ),
     ]);
     return block.receipts[0].result;
   }
@@ -74,40 +86,54 @@ class VaultsManager {
     owner: string,
     token: string,
     debtPayoff: number,
-    prevHint: string,
+    prevHint: string
   ) {
     let block = this.chain.mineBlock([
-      Tx.contractCall("arkadiko-vaults-manager-v1-1", "redeem-vault", [
-        types.principal(Utils.qualifiedName('arkadiko-vaults-tokens-v1-1')),
-        types.principal(Utils.qualifiedName('arkadiko-vaults-data-v1-1')),
-        types.principal(Utils.qualifiedName('arkadiko-vaults-sorted-v1-1')),
-        types.principal(Utils.qualifiedName('arkadiko-vaults-pool-active-v1-1')),
-        types.principal(Utils.qualifiedName('arkadiko-vaults-helpers-v1-1')),
-        types.principal(Utils.qualifiedName('arkadiko-oracle-v2-3')),
-        types.principal(owner),
-        types.principal(Utils.qualifiedName(token)),
-        types.uint(debtPayoff * 1000000),
-        types.some(types.principal(prevHint)),
-      ], caller.address)
+      Tx.contractCall(
+        "arkadiko-vaults-manager-v1-2",
+        "redeem-vault",
+        [
+          types.principal(Utils.qualifiedName("arkadiko-vaults-tokens-v1-1")),
+          types.principal(Utils.qualifiedName("arkadiko-vaults-data-v1-1")),
+          types.principal(Utils.qualifiedName("arkadiko-vaults-sorted-v1-1")),
+          types.principal(
+            Utils.qualifiedName("arkadiko-vaults-pool-active-v1-1")
+          ),
+          types.principal(Utils.qualifiedName("arkadiko-vaults-helpers-v1-1")),
+          types.principal(Utils.qualifiedName("arkadiko-oracle-v2-3")),
+          types.principal(owner),
+          types.principal(Utils.qualifiedName(token)),
+          types.uint(debtPayoff * 1000000),
+          types.some(types.principal(prevHint)),
+        ],
+        caller.address
+      ),
     ]);
     return block.receipts[0].result;
   }
 
   getRedemptionFee(token: string) {
-    return this.chain.callReadOnlyFn("arkadiko-vaults-manager-v1-1", "get-redemption-fee", [
-      types.principal(Utils.qualifiedName('arkadiko-vaults-tokens-v1-1')),
-      types.principal(Utils.qualifiedName(token)),
-    ], this.deployer.address);
+    return this.chain.callReadOnlyFn(
+      "arkadiko-vaults-manager-v1-2",
+      "get-redemption-fee",
+      [
+        types.principal(Utils.qualifiedName("arkadiko-vaults-tokens-v1-1")),
+        types.principal(Utils.qualifiedName(token)),
+      ],
+      this.deployer.address
+    );
   }
 
-  setShutdownActivated(caller: Account, activated: boolean ) {
+  setShutdownActivated(caller: Account, activated: boolean) {
     let block = this.chain.mineBlock([
-      Tx.contractCall("arkadiko-vaults-manager-v1-1", "set-shutdown-activated", [
-        types.bool(activated),
-      ], caller.address)
+      Tx.contractCall(
+        "arkadiko-vaults-manager-v1-2",
+        "set-shutdown-activated",
+        [types.bool(activated)],
+        caller.address
+      ),
     ]);
     return block.receipts[0].result;
   }
-
 }
 export { VaultsManager };
--- scripts/v2/wstx-set-protocol-addresses.js
+++ scripts/v2/wstx-set-protocol-addresses.js
@@ -1,9 +1,9 @@
-require('dotenv').config({path: '../.env'});
+require("dotenv").config({ path: "../.env" });
 const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
-const tx = require('@stacks/transactions');
-const utils = require('../utils');
+const tx = require("@stacks/transactions");
+const utils = require("../utils");
 const network = utils.resolveNetwork();
-const BN = require('bn.js');
+const BN = require("bn.js");
 
 const txOptions = {
   contractAddress: CONTRACT_ADDRESS,
@@ -11,7 +11,7 @@ const txOptions = {
   functionName: "set-protocol-addresses",
   functionArgs: [
     tx.listCV([
-      tx.contractPrincipalCV(CONTRACT_ADDRESS, 'arkadiko-vaults-manager-v1-1'),
+      tx.contractPrincipalCV(CONTRACT_ADDRESS, 'arkadiko-vaults-manager-v1-2'),
       tx.contractPrincipalCV(CONTRACT_ADDRESS, 'arkadiko-vaults-operations-v1-3'),
       tx.contractPrincipalCV(CONTRACT_ADDRESS, 'arkadiko-vaults-pool-active-v1-1'),
       tx.contractPrincipalCV(CONTRACT_ADDRESS, 'arkadiko-vaults-pool-fees-v1-1'),
@@ -24,13 +24,13 @@ const txOptions = {
   fee: new BN(100000, 10),
   senderKey: process.env.STACKS_PRIVATE_KEY,
   postConditionMode: 1,
-  network
+  network,
 };
 
 async function transact() {
   const transaction = await tx.makeContractCall(txOptions);
   const result = tx.broadcastTransaction(transaction, network);
   await utils.processing(result, transaction.txid(), 0);
-};
+}
 
 console.log(transact());
